0f75f4a53a4747141c148dd1cc728df2834c8a67
NinaDang97 <khanh.2dbt@gmail.com>
2022-09-16 17:34:39 +0300

    Feat: set up monorepo with lerna

---
+++ packages/server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { IDataPayload } from './types/data.type';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (_, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const data: IDataPayload = {
+    data: 'Ratatiedot Extranet API',
+  };
+  res.json(data);
+});
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
---
+++ packages/server/src/types/data.type.ts
@@ -0,0 +1,3 @@
+export interface IDataPayload {
+  data: string;
+}
