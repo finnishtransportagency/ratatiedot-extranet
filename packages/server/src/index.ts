1141031d234028c94916a76886edb7638053f15a
khanh.dang@nordcloud.com <khanh.dang@nordcloud.com>
2022-09-20 15:44:01 +0300

    feat: cors config

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,16 +1,33 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+
 import { IDataPayload } from './types/data.type';
 
+dotenv.config();
+const { NODE_ENV, PORT, HOST, FRONTEND_BASE_URL } = process.env;
+
 const app = express();
-const PORT = 8000;
+
+// Define CORS rule
+const whitelist = [FRONTEND_BASE_URL];
+app.use(
+  cors({
+    origin: (origin: string | undefined, callback) => {
+      if ((origin && whitelist.indexOf(origin) !== -1) || typeof origin === 'undefined') {
+        callback(null, true);
+      } else {
+        callback(new Error('Request is blocked due to unlisted resource in our whitelist (CORS)'));
+      }
+    },
+  }),
+);
 
 app.get('/', (_, res) => {
-  // TODO: whitelisting API addresses
-  res.setHeader('Access-Control-Allow-Origin', '*');
   const data: IDataPayload = {
     data: 'Ratatiedot Extranet API',
   };
   res.json(data);
 });
 
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
+app.listen(PORT, () => console.log(`NODE_ENV=${NODE_ENV}\nServer is running at ${HOST}${PORT}`));
