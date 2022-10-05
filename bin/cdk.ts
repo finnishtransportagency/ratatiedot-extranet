0b9d155d996c418d165dbcdaac1af7ef2fcacdf4
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-10-05 16:10:57 +0300

    chore: Removed old argument use

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -3,4 +3,4 @@ import * as cdk from 'aws-cdk-lib';
 import { RataExtraPipelineStack } from '../lib/rataextra-pipeline';
 
 const app = new cdk.App();
-new RataExtraPipelineStack(app, 'RataExtraPipelineStack');
+new RataExtraPipelineStack(app);
