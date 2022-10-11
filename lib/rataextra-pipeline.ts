b43568ce120858e6a14e208b6f8ed206bf18b6bf
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-10-11 14:22:40 +0300

    chore: Added separate build step

--- lib/rataextra-pipeline.ts
+++ lib/rataextra-pipeline.ts
@@ -1,5 +1,5 @@
 import { SecretValue, Stack, Stage, StageProps } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Cache, LocalCacheMode } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 import { getPipelineConfig } from './config';
@@ -19,13 +19,19 @@ export class RataExtraPipelineStack extends Stack {
       tags: config.tags,
     });
 
-    // const buildOutput = new Artifact();
+    const buildAction = new CodeBuildStep('Build', {
+      input: CodePipelineSource.gitHub('finnishtransportagency/ratatiedot-extranet', config.branch, {
+        authentication: SecretValue.secretsManager(config.authenticationToken),
+      }),
+      installCommands: ['npm_config_user=root npm run ci'],
+      commands: ['cd packages/frontend && npm ci && npm run build'],
+      primaryOutputDirectory: './',
+    });
+
     const pipeline = new CodePipeline(this, 'Rataextra-Pipeline', {
       pipelineName: 'Rataextra-' + config.env,
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('finnishtransportagency/ratatiedot-extranet', config.branch, {
-          authentication: SecretValue.secretsManager(config.authenticationToken),
-        }),
+        input: buildAction,
         // TODO: Use when connection is in use
         // input: CodePipelineSource.connection('finnishtransportagency/ratatiedot-extranet', config.branch, {
         //   connectionArn: StringParameter.valueFromLookup(this, config.repoConnectionName),
@@ -33,10 +39,11 @@ export class RataExtraPipelineStack extends Stack {
         commands: [
           'npm_config_user=root npm run ci',
           // 'npm_config_user=root npm run build', // TODO: Lerna symlinking doesn't work in CodePipeline
-          'cd packages/frontend && npm i && npm build', // Testing out separate fe build
-          'ls -lah && cd ../..',
+          // 'cd packages/frontend && npm ci && npm run build', // Testing out separate fe build
+          // 'ls -lah && cd ../..',
           `npm run pipeline:synth --environment=${config.env} --branch=${config.branch}`,
         ],
+        primaryOutputDirectory: './cdk.out',
       }),
       dockerEnabledForSynth: true,
       codeBuildDefaults: {
@@ -44,17 +51,13 @@ export class RataExtraPipelineStack extends Stack {
         cache: Cache.local(LocalCacheMode.CUSTOM, LocalCacheMode.SOURCE, LocalCacheMode.DOCKER_LAYER),
       },
     });
-    // console.log(pipeline.pipeline.artifactBucket);
-    // const deployment = new BucketDeployment(this, 'Deploy FrontEnd', {
-    //   sources: [pipeline.pipeline.artifactBucket],
-    //   destinationBucket: null
-    // });
     pipeline.addStage(new RataExtraApplication(this, 'RataExtra'));
   }
 }
 class RataExtraApplication extends Stage {
+  stack: RataExtraStack;
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-    new RataExtraStack(this, 'Rataextra');
+    this.stack = new RataExtraStack(this, 'Rataextra');
   }
 }
