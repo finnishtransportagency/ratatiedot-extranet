66aa89733616ce804aa1e99fd009a4f99491a30f
NinaDang97 <31354481+NinaDang97@users.noreply.github.com>
2022-10-31 16:10:10 +0200

    chore: remove src folder and dependencies (#41)

--- packages/server/src/controllers/app.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { IDataPayload } from '../types/data.type';
-
-export const getApp = (_: Request, res: Response) => {
-  const data: IDataPayload = {
-    data: 'Ratatiedot Extranet API',
-  };
-  res.json(data);
-};
--- packages/server/src/types/data.type.ts
+++
@@ -1,3 +0,0 @@
-export interface IDataPayload {
-  data: string;
-}
