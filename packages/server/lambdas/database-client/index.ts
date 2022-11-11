2ab7028d7bb4945519f94a07ad709d74b8520567
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-11 17:22:00 +0200

    separate prisma connection to function

---
+++ packages/server/lambdas/database-client/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+//    const encodedConnectionUrl = `postgresql://${databaseName}:${encodeURIComponent(databasePassword)}@${databaseDomain}:5432/${databaseName}?schema=public`;
+
+// Construct evironment variable that prisma uses as connection string to database
+const client = new SSMClient({ region: 'eu-west-1' });
+
+const getParameterByName = (parameterName: string): any => {
+  const command = new GetParameterCommand({ Name: parameterName });
+  client.send(command).then(
+    (data) => {
+      return data.Parameter?.Value;
+    },
+    (error) => {
+      // handle error
+    },
+  );
+};
+
+const secrets = {
+  databaseName: getParameterByName('rataextra-rdspg13-rataextradev-password'),
+  databasePassword: getParameterByName('rataextra-database-name'),
+  databaseDomain: getParameterByName('rataextra-database-domain'),
+};
+
+process.env.DATABASE_URL = `postgresql://${secrets.databaseName}:${encodeURIComponent(secrets.databasePassword)}@${
+  secrets.databaseDomain
+}:5432/${secrets.databaseName}?schema=public`;
+
+export default new PrismaClient();
