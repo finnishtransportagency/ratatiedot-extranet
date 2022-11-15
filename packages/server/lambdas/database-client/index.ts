3085ebe96335f200ec3b7bf6c6aaf3094d9de901
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-15 16:59:36 +0200

    use async/await

--- packages/server/lambdas/database-client/index.ts
+++ packages/server/lambdas/database-client/index.ts
@@ -3,20 +3,21 @@ import { SSM } from '@aws-sdk/client-ssm';
 
 const ssm = new SSM({ region: 'eu-west-1' });
 
-const getParameter = (parameterName?: string): string => {
-  let value = '';
-  ssm.getParameter({ Name: parameterName }).then((data) => {
-    console.log('data: ', data);
-    value = data.Parameter?.Value as string;
-  });
-  console.log('value: ', value);
+const getParameter = async (parameterName?: string): Promise<string | undefined> => {
+  const parameter = await ssm.getParameter({ Name: parameterName });
+  const value = parameter.Parameter?.Value;
+  return value;
+};
+const getSecureStringParameter = async (parameterName?: string): Promise<string | undefined> => {
+  const parameter = await ssm.getParameter({ Name: parameterName });
+  const value = encodeURIComponent(parameter.Parameter?.Value ?? '');
   return value;
 };
 
 console.log('getParameter: ', getParameter(process.env.SSM_DATABASE_NAME_ID));
 
-const DATABASE_URL = `postgresql://${getParameter(process.env.SSM_DATABASE_NAME_ID)}:${encodeURIComponent(
-  getParameter(process.env.SSM_DATABASE_PASSWORD_ID) ?? '',
+const DATABASE_URL = `postgresql://${getParameter(process.env.SSM_DATABASE_NAME_ID)}:${getSecureStringParameter(
+  process.env.SSM_DATABASE_PASSWORD_ID,
 )}@${getParameter(process.env.SSM_DATABASE_DOMAIN_ID)}:5432/${getParameter(
   process.env.SSM_DATABASE_NAME_ID,
 )}?schema=public`;
