ffbfc3e4eb4e0745708d4ef865ba396d3b12d4ac
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-11-15 09:01:44 +0200

    feat: Flip around log message and object Message first

--- packages/server/utils/logger.ts
+++ packages/server/utils/logger.ts
@@ -1,4 +1,4 @@
-import pino from 'pino';
+import pino, { LogFn } from 'pino';
 
 const level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 
@@ -33,6 +33,24 @@ function getLogger(tag: string) {
     },
     // Unix time to ISO time
     timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
+    hooks: {
+      logMethod(inputArgs: any[], method: LogFn) {
+        for (const inputArg of inputArgs) {
+          if (inputArg instanceof Error) {
+            reportError(inputArg);
+          }
+        }
+        // Flip parameters to make log("message", {object with fields}) work insted of log({object with fields}, "message")
+        if (inputArgs.length >= 2) {
+          const arg1 = inputArgs.shift();
+          const arg2 = inputArgs.shift();
+          return method.apply(this, [arg2, arg1, ...inputArgs]);
+        }
+        if (inputArgs[0]) {
+          return method.apply(this, [inputArgs[0]]);
+        }
+      },
+    },
   });
 }
 
