78e6aa1789c8a532a55160f72acbc6c522cd8b6d
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-11-15 10:30:39 +0200

    feat: Added lambda names to log calls Removed old ivalid comments

--- packages/server/lambdas/dummy.ts
+++ packages/server/lambdas/dummy.ts
@@ -2,14 +2,10 @@ import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { log } from '../utils/logger';
 import { getRataExtraLambdaError } from '../utils/errors';
 
-/**
- * DRAFT IMPLEMENTATION
- * Generates a pre-signed url for a file in S3 bucket. Currently takes input in the POST request body.
- */
 export async function handleRequest(event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
   try {
-    log.info('Test log for dummy lambda');
-    log.debug('Sending dummy reply.');
+    log.info('dummyLambda: Test log for dummy lambda');
+    log.debug('dummyLambda: Sending dummy reply.');
     return {
       statusCode: 200,
       headers: {
--- packages/server/lambdas/dummy2.ts
+++ packages/server/lambdas/dummy2.ts
@@ -2,13 +2,9 @@ import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { log } from '../utils/logger';
 import { getRataExtraLambdaError } from '../utils/errors';
 
-/**
- * DRAFT IMPLEMENTATION
- * Generates a pre-signed url for a file in S3 bucket. Currently takes input in the POST request body.
- */
 export async function handleRequest(event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
   try {
-    log.debug('Sending dummy2 reply.');
+    log.debug('dummy2Lambda: Sending dummy2 reply.');
     return {
       statusCode: 200,
       headers: {
