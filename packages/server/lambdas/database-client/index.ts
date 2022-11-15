a2f9e4190dfc71943c88211cc11c175ca859c729
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-15 15:57:31 +0200

    log env to see if exists

--- packages/server/lambdas/database-client/index.ts
+++ packages/server/lambdas/database-client/index.ts
@@ -15,10 +15,15 @@ const getParameter = (parameterName?: string): string | void => {
     });
 };
 
-process.env.DATABASE_URL = `postgresql://${getParameter(process.env.SSM_DATABASE_NAME_ID)}:${encodeURIComponent(
+const DATABASE_URL = `postgresql://${getParameter(process.env.SSM_DATABASE_NAME_ID)}:${encodeURIComponent(
   getParameter(process.env.SSM_DATABASE_PASSWORD_ID) ?? '',
 )}@${getParameter(process.env.SSM_DATABASE_DOMAIN_ID)}:5432/${getParameter(
   process.env.SSM_DATABASE_NAME_ID,
 )}?schema=public`;
 
-export default new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
+process.env.DATABASE_URL = 'test_value';
+
+console.log('testENV: ', process.env.DATABASE_URL);
+console.log('constant: ', DATABASE_URL);
+
+export default new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
