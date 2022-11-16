c5d4d1e0ea96acd1abc7239a503bb988c60f515f
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-16 10:30:34 +0200

    unused parameter blocked build

--- packages/server/lambdas/dummy.ts
+++ packages/server/lambdas/dummy.ts
@@ -6,7 +6,7 @@ import { getRataExtraLambdaError } from '../utils/errors';
  * DRAFT IMPLEMENTATION
  * Generates a pre-signed url for a file in S3 bucket. Currently takes input in the POST request body.
  */
-export async function handleRequest(event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
+export async function handleRequest(_event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
   try {
     return {
       statusCode: 200,
