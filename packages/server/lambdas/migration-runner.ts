88387bc2aca9694f3ebe8d422b8f65c7c611393b
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-17 16:35:16 +0200

    add async exec

--- packages/server/lambdas/migration-runner.ts
+++ packages/server/lambdas/migration-runner.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import { APIGatewayEvent, Context } from 'aws-lambda';
 import { SSM } from '@aws-sdk/client-ssm';
 
@@ -27,23 +27,30 @@ export async function handleRequest(_event: APIGatewayEvent, _context: Context) 
     return value;
   };
 
-  const executeMigration = async () => {
-    const [databaseName, databaseDomain, databasePassword] = await Promise.all([
-      getParameter(process.env.SSM_DATABASE_NAME_ID),
-      getParameter(process.env.SSM_DATABASE_DOMAIN_ID),
-      getSecureStringParameter(process.env.SSM_DATABASE_PASSWORD_ID),
-    ]);
+  const [databaseName, databaseDomain, databasePassword] = await Promise.all([
+    getParameter(process.env.SSM_DATABASE_NAME_ID),
+    getParameter(process.env.SSM_DATABASE_DOMAIN_ID),
+    getSecureStringParameter(process.env.SSM_DATABASE_PASSWORD_ID),
+  ]);
 
-    console.log('database domain: ', databaseDomain);
-    const child = spawn(
+  console.log('database domain: ', databaseDomain);
+  const promise = new Promise((resolve, reject) => {
+    exec(
       `DATABASE_URL="postgresql://${databaseName}:${databasePassword}@${databaseDomain}:5432/${databaseName}?schema=public}" npx prisma migrate deploy --schema prisma/schema.prisma`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`error: ${error.message}`);
+          return 'Error 500 running migration.';
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return 'Error 2 500 running migration.';
+        }
+        console.log(`stdout: ${stdout}`);
+        return 'Migration successfull.';
+      },
     );
-    child.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
-    child.on('error', function (err) {
-      console.log('Prisma migration failed: ' + err);
-    });
-  };
-  executeMigration();
+  });
+
+  return promise;
 }
