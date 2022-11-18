6a9769690a2e4963c995f695a86390c36ea4b50d
NinaDang97 <khanh.2dbt@gmail.com>
2022-11-18 22:24:53 +0200

    aineistoluokka data helper functions

--- packages/frontend/src/utils/helpers.ts
+++ packages/frontend/src/utils/helpers.ts
@@ -1,3 +1,15 @@
-import { Viewports } from '../constants/Viewports';
+export const generateYearsBetween = (startYear: number, endYear: number = new Date().getFullYear()) => {
+  let years = [];
+  while (startYear <= endYear) {
+    years.push(startYear);
+    startYear++;
+  }
+  return years;
+};
 
-export const isDesktopScreen = window.innerWidth >= Viewports.DESKTOP;
+export const flatMapByKey = (array: Array<any>, key: any): Array<any> => {
+  return array.reduce((acc: Array<any>, next: Array<any>) => {
+    if (!next[key]) return acc;
+    return (acc = [...next[key], ...acc]);
+  }, []);
+};
