edd499e3fe592dcdcce7d22940a13ba95a68a0f5
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-11-21 08:52:28 +0200

    feat: Undoing previous change

--- packages/server/utils/userService.ts
+++ packages/server/utils/userService.ts
@@ -25,7 +25,7 @@ function parseRoles(roles: string): string[] | undefined {
     : undefined;
 }
 
-const isReadUser = (user: User) => user.roles?.includes('Rataextra_lukuDUMMY');
+const isReadUser = (user: User) => user.roles?.includes('Rataextra_luku');
 
 export const validateReadUser = async (event: APIGatewayEvent): Promise<void> => {
   log.debug('dummy2Lambda: Validating read user');
