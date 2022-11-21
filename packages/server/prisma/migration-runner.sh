5fb04675a027b998f9857e4740e1542cbab68859
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-11-21 12:47:59 +0200

    remove hardcoded values

--- packages/server/prisma/migration-runner.sh
+++ packages/server/prisma/migration-runner.sh
@@ -8,27 +8,19 @@ do
 done
 
 database_name=$(aws ssm get-parameter \
-    --region eu-west-1 \
-    --profile 178238255639_RataextraAdmin \
     --name $name \
     --query 'Parameter.Value' \
     --output text)
 database_domain=$(aws ssm get-parameter \
-    --region eu-west-1 \
-    --profile 178238255639_RataextraAdmin \
     --name $domain \
     --query 'Parameter.Value' \
     --output text)
 database_password=$(aws ssm get-parameter \
-    --region eu-west-1 \
-    --profile 178238255639_RataextraAdmin \
     --name $password \
     --with-decryption \
     --query 'Parameter.Value' \
     --output text)
 
 DATABASE_URL='postgresql://'${database_name}':'${database_password}'@'${database_domain}'5432/'${database_name}'?schema=public'
-
-ls -la
 
 npx prisma migrate deploy
