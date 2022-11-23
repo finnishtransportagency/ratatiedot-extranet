b6bfb1032dc792c97e767a5189b020b7a4f34bd1
NinaDang97 <khanh.2dbt@gmail.com>
2022-11-23 13:14:41 +0200

    Add search result page

--- packages/frontend/src/components/Search/index.tsx
+++ packages/frontend/src/components/Search/index.tsx
@@ -8,6 +8,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import { SearchContext } from '../../contexts/SearchContext';
 import { RecentSearch } from './RecentSearch';
 import { KeyEnum, LocalStorageHelper } from '../../utils/StorageHelper';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from '../../constants/Routes';
 
 type SearchProps = {
   openSearch: boolean;
@@ -18,14 +20,21 @@ type SearchProps = {
 export const Search = ({ openSearch, toggleSearch, isDesktop = false }: SearchProps) => {
   const searchContext = useContext(SearchContext);
   const { query, queryHandler } = searchContext;
+  const navigate = useNavigate();
 
   // Set limit for number of searches
   const SearchStorage = new LocalStorageHelper(5);
 
+  const closeSearch = () => {
+    openSearch && toggleSearch();
+  };
+
   const enterSearch = (event: React.KeyboardEvent) => {
     if (event.code === 'Enter' && query) {
       SearchStorage.add(KeyEnum.RECENT_SEARCHES, query);
+      closeSearch();
       // TODO: Navigate to new SearchPage (path /haku)
+      navigate(`${Routes.SEARCH_RESULT}?query=${query}`);
     }
   };
 
