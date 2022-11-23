0a69034be724482cdacde37be5186060278ebb75
NinaDang97 <khanh.2dbt@gmail.com>
2022-11-23 13:19:21 +0200

    Add real life test case as group of 10 years

--- packages/frontend/src/utils/__tests__/helpers.test.ts
+++ packages/frontend/src/utils/__tests__/helpers.test.ts
@@ -25,6 +25,16 @@ describe('Helpers Utility', () => {
       const years = generateYearsBetween(2018, 2022);
       expect(splitYearsIntoChunks(years, 2)).toEqual([[2018, 2019], [2020, 2021], [2022]]);
     });
+    it('should generate a 2-dimensional array of group of 10', () => {
+      const years = generateYearsBetween(1980, 2022);
+      expect(splitYearsIntoChunks(years, 10)).toEqual([
+        [1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989],
+        [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+        [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009],
+        [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+        [2020, 2021, 2022],
+      ]);
+    });
     it('should return empty array if `perChunk` is negative', () => {
       const years = generateYearsBetween(2018, 2022);
       expect(splitYearsIntoChunks(years, -1)).toEqual([]);
