e2472f83156d589529886437727dd9b2d976a137
NinaDang97 <khanh.2dbt@gmail.com>
2022-11-28 12:16:39 +0200

    Import Tag component in SearchResult

--- packages/frontend/src/pages/Search/SearchResult.tsx
+++ packages/frontend/src/pages/Search/SearchResult.tsx
@@ -1,30 +1,16 @@
 import { Typography } from '@mui/material';
-import axios from 'axios';
-import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { Tags } from '../../components/Tags';
 import { ContainerWrapper } from '../Landing/index.styles';
 
 export const SearchResult = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
-  useEffect(() => {
-    const searchDocs = async () => {
-      try {
-        const url = ` https://testiextranet.vayla.fi/alfresco/api/-default-/public/alfresco/versions/1/queries/nodes?term=${query}`;
-        const data = await fetch(url, {
-          headers: {
-            Cookie: document.cookie,
-          },
-        });
-        console.log(data);
-      } catch (e) {}
-    };
 
-    searchDocs();
-  });
   return (
     <ContainerWrapper>
       <Typography variant="subtitle1">Hakutulokset "{query}"</Typography>
+      <Tags />
     </ContainerWrapper>
   );
 };
