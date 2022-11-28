8bd25951ce9138e6c839e59c706a798f0b32cc6b
NinaDang97 <khanh.2dbt@gmail.com>
2022-11-28 14:13:01 +0200

    Add node list to Search result page

--- packages/frontend/src/pages/Search/SearchResult.tsx
+++ packages/frontend/src/pages/Search/SearchResult.tsx
@@ -3,6 +3,10 @@ import { useSearchParams } from 'react-router-dom';
 import { Tags } from '../../components/Tags';
 import { ContainerWrapper } from '../Landing/index.styles';
 
+// TODO: Use real data fetched from Backend (currently use mock data)
+import mockData from './mockData.json';
+import { NodeItem } from './NodeItem';
+
 export const SearchResult = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
@@ -11,6 +15,12 @@ export const SearchResult = () => {
     <ContainerWrapper>
       <Typography variant="subtitle1">Hakutulokset "{query}"</Typography>
       <Tags />
+      <Typography variant="body1" sx={{ margin: '24px 0px' }}>
+        {mockData.list.pagination.totalItems} tulosta
+      </Typography>
+      {mockData.list.entries.map((node: any, index: number) => (
+        <NodeItem key={index} row={index} node={node} />
+      ))}
     </ContainerWrapper>
   );
 };
