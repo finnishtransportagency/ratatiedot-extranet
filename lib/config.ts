0b12758cf9073a3a23838b9e36a27018261a651e
Niko Mehtonen <nikomehtonen@gmail.com>
2022-12-05 12:59:23 +0200

    Feat: RTENU-87 Fix return_url decode, remove awsaccount from pipeline (#88)
    * fix: Fix return_url decoding
    
    * ci: Removed awsaccount from CI pipeline
    Not needed anymore

--- lib/config.ts
+++ lib/config.ts
@@ -65,10 +65,8 @@ export const getPipelineConfig = () => {
   const env = getEnvOrFail('ENVIRONMENT');
   if (isRataExtraEnvironment(env)) {
     const branch = env === ENVIRONMENTS.prod ? PRODUCTION_BRANCH : getEnvOrFail('BRANCH');
-    const account = getEnvOrFail('AWS_ACCOUNT');
     return {
       env,
-      account,
       branch,
       stackId: getStackId(branch),
       authenticationToken: 'github-token',
