7ec78766c0800758546cc6aace4a2f943f7e8f17
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-12-07 15:48:13 +0200

    fix lambda

--- packages/server/lambdas/list-users.ts
+++ packages/server/lambdas/list-users.ts
@@ -1,12 +1,12 @@
 import { ALBEvent, Context } from 'aws-lambda';
 // import { getRataExtraLambdaError } from '../utils/errors.js';
-import { log } from '../utils/logger.js';
+// import { log } from '../utils/logger.js';
 // import { getUser, validateReadUser } from '../utils/userService.js';
 import { DatabaseClient } from './database-client/index.js';
 
 const database = await DatabaseClient.build();
 
-export async function handleRequest(event: ALBEvent, _context: Context) {
+export async function handleRequest(_event: ALBEvent, _context: Context) {
   //  try {
   //    const user = await getUser(event);
   //    await validateReadUser(user);
@@ -14,20 +14,21 @@ export async function handleRequest(event: ALBEvent, _context: Context) {
   //    log.error(err);
   //    return getRataExtraLambdaError(err);
   //  }
-  await database.user
-    .findMany({ take: 10 })
-    .then((res) => {
-      const response = {
-        statusCode: 200,
-        headers: {
-          my_header: 'my_value',
-        },
-        body: JSON.stringify(res),
-        isBase64Encoded: false,
-      };
-      return response;
-    })
-    .catch(async (e) => {
-      log.error(e);
+  try {
+    const users = await database.user.findMany({
+      include: { profile: true },
     });
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(users),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(error),
+    };
+  }
 }
