8f190f20ff14fdf821091c111b3a17fb5ebaa64e
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-12-12 09:12:49 +0200

    remove unused package

--- packages/frontend/src/i18n.ts
+++ packages/frontend/src/i18n.ts
@@ -2,7 +2,6 @@
 // Translation files for other languages can be found in public directory.
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import common from './assets/locales/fi/common.json';
 import landing from './assets/locales/fi/landing.json';
@@ -15,7 +14,7 @@ export const resources = {
   },
 };
 
-i18next.use(LanguageDetector).use(initReactI18next).init({
+i18next.use(initReactI18next).init({
   lng: 'fi', // override any language detector and use Finnish as default. Remove this if you need localization in the future.
   fallbackLng: 'fi',
   debug: true,
