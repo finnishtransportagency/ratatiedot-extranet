eb21f64589989bae39e331cfa47a56a73a238280
NinaDang97 <khanh.2dbt@gmail.com>
2022-12-14 14:54:52 +0200

    Pass body request to query builder

--- packages/server/lambdas/alfresco/search.ts
+++ packages/server/lambdas/alfresco/search.ts
@@ -5,14 +5,25 @@ import { getRataExtraLambdaError } from '../../utils/errors';
 import { log } from '../../utils/logger';
 import { getParameter, getSecuredStringParameter } from '../../utils/parameterStore';
 import { getUser, validateReadUser } from '../../utils/userService';
+import { searchQueryBuilder } from './searchQueryBuilder';
 
 let alfrescoAPIKey: string | null = null;
 let alfrescoAPIUrl: string | null = null;
 const alfrescoAPIKeyName = process.env.ALFRESCO_API_KEY || '';
 const alfrescoAPIUrlName = process.env.ALFRESCO_API_URL || '';
 
-const searchByTerm = async (cookie: string, term: string) => {
+const searchByTerm = async ({ headers, body }: ALBEvent) => {
   try {
+    // Testing `cookie` and `body` by event.json file
+    const cookie = headers?.cookie || '';
+    // ALBEvent's body type is string | null
+    const parsedBody = body ? JSON.parse(body) : {};
+    const bodyRequest = searchQueryBuilder({
+      searchParameters: parsedBody.searchParameters,
+      page: parsedBody.page,
+      language: parsedBody.language,
+    });
+
     if (!alfrescoAPIKey) {
       alfrescoAPIKey = await getSecuredStringParameter(alfrescoAPIKeyName);
     }
@@ -23,9 +34,10 @@ const searchByTerm = async (cookie: string, term: string) => {
       headers: {
         'X-API-Key': alfrescoAPIKey,
         Cookie: cookie,
+        'Content-Type': 'application/json;charset=UTF-8',
       },
     };
-    const response = await axios.get(`${alfrescoAPIUrl}/queries/nodes?term=${term}`, options);
+    const response = await axios.post(`${alfrescoAPIUrl}/search`, bodyRequest, options);
     return response.data;
   } catch (err) {
     log.error(err);
@@ -37,14 +49,7 @@ export async function handleRequest(event: ALBEvent, _context: Context) {
   try {
     const user = await getUser(event);
     await validateReadUser(user);
-    let data = {};
-    const term = event.queryStringParameters?.term ? decodeURIComponent(event.queryStringParameters.term) : '';
-    // Testing cookie by event.json file
-    const cookie = event.headers?.cookie || '';
-
-    if (term) {
-      data = await searchByTerm(cookie, term);
-    }
+    const data = await searchByTerm(event);
     return {
       statusCode: 200,
       headers: {
