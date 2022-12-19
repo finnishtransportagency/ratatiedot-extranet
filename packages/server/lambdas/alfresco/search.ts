29bd424dbf885d2c16fd9f329e559215bef8de4c
NinaDang97 <khanh.2dbt@gmail.com>
2022-12-19 14:20:14 +0200

    Logging parsed body

--- packages/server/lambdas/alfresco/search.ts
+++ packages/server/lambdas/alfresco/search.ts
@@ -18,11 +18,13 @@ const searchByTerm = async (body: string | null, uid: string) => {
     // ALBEvent's body type is string | null
     log.info(body, 'POST body request');
     const parsedBody = body ? JSON.parse(body) : {};
+    log.info(parsedBody, 'Body parsing...');
     const bodyRequest = searchQueryBuilder({
       searchParameters: parsedBody.searchParameters,
       page: parsedBody.page,
       language: parsedBody.language,
     });
+    log.info(bodyRequest, 'Complete body request');
 
     if (!alfrescoAPIKey) {
       alfrescoAPIKey = await getSecuredStringParameter(alfrescoAPIKeyName);
