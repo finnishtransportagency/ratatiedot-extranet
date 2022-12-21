b96b96623690ece52b594512c86b7f022393f269
Santeri Pigg <santeri.pigg@nordcloud.com>
2022-12-21 16:52:29 +0200

    clear cookies before logout

--- packages/frontend/src/routes.tsx
+++ packages/frontend/src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import { createBrowserRouter, RouteObject, redirect } from 'react-router-dom';
 
 import { Landing } from './pages/Landing';
 import { Routes } from './constants/Routes';
@@ -32,6 +32,19 @@ const routes: RouteObject[] = [
       // TODO: throw error if user has no permission
     },
     children: [],
+  },
+  {
+    path: Routes.LOGOUT,
+    element: <div>LOGGING OUT...</div>,
+    errorElement: <RootBoundary />, // Send user here whenever error is thrown
+    loader: async () => {
+      console.log('Logging out');
+      document.cookie = 'Return=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      // check if cookie exists then redirect to /
+      if (document.cookie.indexOf('Return') === -1) {
+        return redirect(Routes.LOGOUT_REDIRECT);
+      }
+    },
   },
 ];
 
