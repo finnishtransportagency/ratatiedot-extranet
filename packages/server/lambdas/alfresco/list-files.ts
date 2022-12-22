28650866d40e9c0f95e32752d7214175079c003a
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-12-22 13:14:06 +0200

    feat: Use existing search for listing files on page

--- packages/server/lambdas/alfresco/list-files.ts
+++ packages/server/lambdas/alfresco/list-files.ts
@@ -4,10 +4,39 @@ import axios from 'axios';
 
 import { getRataExtraLambdaError, RataExtraLambdaError } from '../../utils/errors';
 import { log } from '../../utils/logger';
-import { findEndpoint, getAlfrescoAncestor, getAlfrescoOptions, getAlfrescoUrlBase } from '../../utils/alfresco';
+import { findEndpoint, getAlfrescoOptions, getAlfrescoUrlBase } from '../../utils/alfresco';
 import { getUser, validateReadUser } from '../../utils/userService';
 import { DatabaseClient } from '../database/client';
+import { searchQueryBuilder } from './searchQueryBuilder';
+import { Include, IParentSearchParameter, SearchParameter, SearchParameterName } from './searchQueryBuilder/types';
 
+const searchByTermWithParent = async (body: string | null, uid: string, alfrescoParent: string) => {
+  try {
+    const parsedBody = body ? JSON.parse(body) : {};
+
+    const searchparameters: Array<SearchParameter> = parsedBody.searchParameters;
+    const parent: IParentSearchParameter = {
+      parameterName: SearchParameterName.PARENT,
+      parent: alfrescoParent,
+    };
+    searchparameters.concat(parent);
+
+    const bodyRequest = searchQueryBuilder({
+      searchParameters: searchparameters,
+      page: parsedBody.page,
+      language: parsedBody.language,
+      include: [Include.PROPERTIES],
+    });
+
+    const alfrescoAPIUrl = getAlfrescoUrlBase();
+    const options = await getAlfrescoOptions(uid, { 'Content-Type': 'application/json;charset=UTF-8' });
+
+    const response = await axios.post(`${alfrescoAPIUrl}/search`, bodyRequest, options);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
 // TODO: Preliminary implementation based on search.ts
 // Not tested, so won't likely work
 const database = await DatabaseClient.build();
@@ -26,22 +55,17 @@ export async function handleRequest(event: ALBEvent, _context: Context) {
     if (!fileEndpointsCache.length) {
       fileEndpointsCache = await database.categoryDataBase.findMany();
     }
-    const alfrescoEndpoint = findEndpoint(category, fileEndpointsCache)?.alfrescoFolder;
-    if (!alfrescoEndpoint) {
+    const alfrescoParent = findEndpoint(category, fileEndpointsCache)?.alfrescoFolder;
+    if (!alfrescoParent) {
       throw new RataExtraLambdaError('Category not found', 404);
     }
-
-    const options = await getAlfrescoOptions(user.uid);
-    const alfrescoAPIUrlBase = getAlfrescoUrlBase();
-    const alfrescoAPIPath = getAlfrescoAncestor();
-    // TODO: Not working
-    const response = await axios.get(`${alfrescoAPIUrlBase}${alfrescoAPIPath}${alfrescoEndpoint}`, options);
+    const data = await searchByTermWithParent(event.body, user.uid, alfrescoParent);
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(response.data),
+      body: JSON.stringify(data),
     };
   } catch (err) {
     log.error(err);
