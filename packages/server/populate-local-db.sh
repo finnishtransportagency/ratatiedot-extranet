b054417ea1a5ea7a301dd775bb1259736fda3f61
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-12-23 08:05:53 +0200

    feat: Updated local db population script With proper values

--- packages/server/populate-local-db.sh
+++ packages/server/populate-local-db.sh
@@ -4,7 +4,7 @@ psql -h $LOCAL_IP -p $DB_PORT -d $DATABASE_NAME -U $DATABASE_USER << END_OF_SCRI
 TRUNCATE "CategoryDataContents", "CategoryDataBase" restart identity;
 
 INSERT INTO "CategoryDataBase" ("rataextraRequestPage", "alfrescoFolder", "writeRights")
-VALUES ('/linjakaaviot', '/123', 'Rataextra_kirjoitus_linjakaaviot');
+VALUES ('linjakaaviot', '6e49cab2-b923-420c-99cc-5926cb0a6375', 'Rataextra_kirjoitus_linjakaaviot');
 
 INSERT INTO "CategoryDataContents" ("baseId", "fields")
 VALUES (1, '{"test": "test"}')
