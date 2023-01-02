6ab4074a36e71756ec94ae504e61236f9594c53a
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2023-01-02 15:43:06 +0200

    feat: Update page contents Preliminary implementation

---
+++ packages/server/lambdas/database/update-page-contents.ts
@@ -0,0 +1,62 @@
+import { CategoryDataBase } from '@prisma/client';
+import { ALBEvent, ALBResult } from 'aws-lambda';
+import { findEndpoint } from '../../utils/alfresco';
+
+import { getRataExtraLambdaError, RataExtraLambdaError } from '../../utils/errors';
+import { log } from '../../utils/logger';
+import { getUser, validateReadUser, validateWriteUser } from '../../utils/userService';
+import { DatabaseClient } from './client';
+
+type UpdateRequestBody = {
+  category: string;
+  fields: Record<string, string>;
+};
+
+const database = await DatabaseClient.build();
+
+let fileEndpointsCache: Array<CategoryDataBase> = [];
+
+/**
+ * Update custom content for page
+ * @param {ALBEvent} event
+ * @param {UpdateRequestBody} event.body Page to get the custom content for
+ * @returns  {Promise<ALBResult>} JSON stringified object of contents inside body
+ */
+export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
+  try {
+    const user = await getUser(event);
+    const body: UpdateRequestBody = event.body ? JSON.parse(event.body) : {};
+    const category = body.category;
+    log.info(user, `Updating page contents for page ${body.category}`);
+    validateReadUser(user);
+    if (!category) {
+      throw new RataExtraLambdaError('Category missing', 400);
+    }
+    if (!fileEndpointsCache.length) {
+      fileEndpointsCache = await database.categoryDataBase.findMany();
+    }
+    const categoryData = findEndpoint(category, fileEndpointsCache);
+    if (!categoryData) {
+      throw new RataExtraLambdaError('Category not found', 404);
+    }
+
+    const writeRole = categoryData.writeRights;
+    validateWriteUser(user, writeRole);
+
+    const contents = await database.categoryDataContents.update({
+      where: { baseId: categoryData.id },
+      data: { fields: body.fields },
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ fields: contents?.fields }),
+    };
+  } catch (err) {
+    log.error(err);
+    return getRataExtraLambdaError(err);
+  }
+}
