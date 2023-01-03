2d0b47a18bb8ffda12b5e4575769f09a96cbe786
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2023-01-03 11:11:57 +0200

    feat: Added local database testing to lambdas

--- packages/server/lambdas/database/client/index.ts
+++ packages/server/lambdas/database/client/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { getParameter, getSecuredStringParameter } from '../../../utils/parameterStore';
 
+const environment = process.env.ENVIRONMENT;
 const databaseNameId = process.env.SSM_DATABASE_NAME_ID || '';
 const databaseDomainId = process.env.SSM_DATABASE_DOMAIN_ID || '';
 const databasePasswordId = process.env.SSM_DATABASE_PASSWORD_ID || '';
@@ -11,15 +12,20 @@ let databasePassword: string | null = null;
 
 export class DatabaseClient {
   public static async build(): Promise<PrismaClient> {
-    if (!databaseName || !databaseDomain || !databasePassword) {
-      [databaseName, databaseDomain, databasePassword] = await Promise.all([
-        getParameter(databaseNameId),
-        getParameter(databaseDomainId),
-        getSecuredStringParameter(databasePasswordId),
-      ]);
-    }
+    let DATABASE_URL: string;
+    if (environment === 'local') {
+      DATABASE_URL = 'postgresql://root:root@host.docker.internal:5432/test_db?schema=public';
+    } else {
+      if (!databaseName || !databaseDomain || !databasePassword) {
+        [databaseName, databaseDomain, databasePassword] = await Promise.all([
+          getParameter(databaseNameId),
+          getParameter(databaseDomainId),
+          getSecuredStringParameter(databasePasswordId),
+        ]);
+      }
 
-    const DATABASE_URL = `postgresql://${databaseName}:${databasePassword}@${databaseDomain}:5432/${databaseName}?schema=public`;
+      DATABASE_URL = `postgresql://${databaseName}:${databasePassword}@${databaseDomain}:5432/${databaseName}?schema=public`;
+    }
 
     return new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
   }
