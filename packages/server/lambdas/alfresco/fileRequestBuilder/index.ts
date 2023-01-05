77e7f5d08aae3b8eaf314c0dfdc33af799cc82f9
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2023-01-05 15:25:13 +0200

    refactor: Added missing FileStore support

--- packages/server/lambdas/alfresco/fileRequestBuilder/index.ts
+++ packages/server/lambdas/alfresco/fileRequestBuilder/index.ts
@@ -1,9 +1,13 @@
 import { AlfrescoFileRequestBuilder } from './alfrescoRequestBuilder';
 import { FileDeleteRequest, FileDeleteRequestBody, FileRequest, IFileRequestBody, FileStore } from './types';
 
-export const fileRequestBuilder = (request: IFileRequestBody): FileRequest => {
-  const alfrescoRequestBuilder = new AlfrescoFileRequestBuilder();
-  return alfrescoRequestBuilder.requestBuilder(request);
+export const fileRequestBuilder = (request: IFileRequestBody, store?: FileStore): FileRequest => {
+  switch (store) {
+    case FileStore.ALFRESCO:
+    default:
+      const alfrescoRequestBuilder = new AlfrescoFileRequestBuilder();
+      return alfrescoRequestBuilder.requestBuilder(request);
+  }
 };
 export const deleteFileRequestBuilder = (request: FileDeleteRequestBody, store?: FileStore): FileDeleteRequest => {
   switch (store) {
