9033ec18557cc0002d46d24cdfa945e950d5bd8a
Santeri Pigg <santeri.pigg@nordcloud.com>
2023-01-23 18:17:11 +0200

    install node-fetch
    install node-fetch
    
    import fetch
    
    fix async stuff

--- packages/server/lambdas/alfresco/fileRequestBuilder/alfrescoRequestBuilder.ts
+++ packages/server/lambdas/alfresco/fileRequestBuilder/alfrescoRequestBuilder.ts
@@ -2,36 +2,46 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { getAlfrescoUrlBase } from '../../../utils/alfresco';
 import { FileDeleteRequest, FileDeleteRequestBody, IFileRequestBody, IFileResponse } from './types';
 import { FormData } from 'formdata-node';
+import fetch from 'node-fetch';
 
 const dataToBlob = async (data: string) => {
   const base64 = await fetch(data);
-  const blob = await base64.blob();
+  const blob = base64.blob();
   return blob;
 };
+const postFile = async (data: FormData, nodeId: string) => {
+  await axios({
+    method: 'post',
+    url: `${getAlfrescoUrlBase()}/nodes/${nodeId}/children`,
+    data: data,
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+};
+
 export class AlfrescoFileRequestBuilder {
-  public async requestBuilder(nodeId: string, requestBody: string): Promise<AxiosResponse<IFileResponse> | AxiosError> {
-    const bodyFormData = new FormData();
+  public async requestBuilder(
+    nodeId: string,
+    requestBody: string,
+  ): Promise<AxiosResponse<IFileResponse> | AxiosError | void> {
+    dataToBlob(requestBody)
+      .then((blob) => {
+        const bodyFormData = new FormData();
 
-    const blob = dataToBlob(requestBody);
-    console.log('blob: ', blob);
-    bodyFormData.append('filedata', blob, Date.now().toString() + '_FILE.txt');
-    console.log('bodyFormData -> ', bodyFormData);
+        bodyFormData.append('filedata', blob, Date.now().toString() + '_FILE.txt');
 
-    const response = await axios({
-      method: 'post',
-      url: `${getAlfrescoUrlBase()}/nodes/${nodeId}/children`,
-      data: bodyFormData,
-      headers: { 'Content-Type': 'multipart/form-data' },
-    })
-      .then((response) => {
-        console.log(response);
-        return response;
+        postFile(bodyFormData, nodeId)
+          .then((response) => {
+            console.log('RESPONSE1: ', response);
+            return response;
+          })
+          .catch((error: AxiosError) => {
+            console.log(error);
+            return error;
+          });
       })
-      .catch((error: AxiosError) => {
-        console.log(error);
+      .catch((error) => {
         return error;
       });
-    return response;
   }
   public deleteRequestBuilder(requestParameters: FileDeleteRequestBody): FileDeleteRequest {
     throw new Error('Method not implemented.');
