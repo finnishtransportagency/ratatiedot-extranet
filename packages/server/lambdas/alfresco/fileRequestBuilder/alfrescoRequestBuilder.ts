eec49f07411f543eb2b7f6c83ba1be72892bf06a
Santeri Pigg <santeri.pigg@nordcloud.com>
2023-01-23 16:41:46 +0200

    replace library by spec-compilant implementation

--- packages/server/lambdas/alfresco/fileRequestBuilder/alfrescoRequestBuilder.ts
+++ packages/server/lambdas/alfresco/fileRequestBuilder/alfrescoRequestBuilder.ts
@@ -1,23 +1,24 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { getAlfrescoUrlBase } from '../../../utils/alfresco';
 import { FileDeleteRequest, FileDeleteRequestBody, IFileRequestBody, IFileResponse } from './types';
-import FormData from 'form-data';
+import { FormData } from 'formdata-node';
 
 export class AlfrescoFileRequestBuilder {
   public async requestBuilder(nodeId: string, requestBody: string): Promise<AxiosResponse<IFileResponse> | AxiosError> {
     const bodyFormData = new FormData();
-    console.log('bodyFormData -> ', bodyFormData);
     // for (const key in requestBody) {
     //   if (Object.prototype.hasOwnProperty.call(requestBody, key)) {
     //     bodyFormData.append(key, requestBody[key as keyof IFileRequestBody]);
     //   }
     // }
-    bodyFormData.append('filedata', requestBody);
+    const buffer = Buffer.from(requestBody, 'base64');
+    bodyFormData.append('filedata', buffer, Date.now().toString() + '_FILE.txt');
+    console.log('bodyFormData -> ', bodyFormData);
 
     const response = await axios({
       method: 'post',
       url: `${getAlfrescoUrlBase()}/nodes/${nodeId}/children`,
-      data: requestBody,
+      data: bodyFormData,
       headers: { 'Content-Type': 'multipart/form-data' },
     })
       .then((response) => {
