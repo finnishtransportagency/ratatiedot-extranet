cc7f316e4c6edd2069c1a4007fcd06462b26a4a2
NinaDang97 <khanh.2dbt@gmail.com>
2023-02-03 09:36:15 +0200

    Change subcategory page router name

--- packages/frontend/src/constants/Routes.ts
+++ packages/frontend/src/constants/Routes.ts
@@ -1,5 +1,6 @@
-import { getRouterName, getSubCategoryData } from '../utils/helpers';
+import { getMainCategoryData, getRouterName, getSubCategoryData } from '../utils/helpers';
 
+const MainCategoryNames = getMainCategoryData();
 const SubCategoryNames = getSubCategoryData();
 
 export const Routes = {
@@ -9,25 +10,39 @@ export const Routes = {
   SEARCH_RESULT: '/haku',
   LOGOUT: 'logout',
   LOGOUT_REDIRECT: '/sso/*',
-  LINE_DIAGRAMS: `/${getRouterName(SubCategoryNames.LINE_DIAGRAMS)}`,
-  SPEED_DIAGRAMS: `/${getRouterName(SubCategoryNames.SPEED_DIAGRAMS)}`,
-  TRACK_DIAGRAMS: `/${getRouterName(SubCategoryNames.TRACK_DIAGRAMS)}`,
-  GROUPING_DIAGRAMS: `/${getRouterName(SubCategoryNames.GROUPING_DIAGRAMS)}`,
-  INTERCHANGE_DECISIONS: `/${getRouterName(SubCategoryNames.INTERCHANGE_DECISIONS)}`,
-  RAILWAY_SIGNS: `/${getRouterName(SubCategoryNames.RAILWAY_SIGNS)}`,
-  RAILWAY_ASSET_NUMBERS: `/${getRouterName(SubCategoryNames.RAILWAY_ASSET_NUMBERS)}`,
-  RAILWAY_MAPS: `/${getRouterName(SubCategoryNames.RAILWAY_MAPS)}`,
-  RAILWAY_INTERCHANGE_DEVELOPMENT_NEEDS: `/${getRouterName(SubCategoryNames.RAILWAY_INTERCHANGE_DEVELOPMENT_NEEDS)}`,
-  ROUTE_DOCUMENTS: `/${getRouterName(SubCategoryNames.ROUTE_DOCUMENTS)}`,
-  RINF_REGISTER: `/${getRouterName(SubCategoryNames.RINF_REGISTER)}`,
-  VAK_RAIL_DEPOT: `/${getRouterName(SubCategoryNames.VAK_RAIL_DEPOT)}`,
-  BRIDGE_INSPECTIONS: `/${getRouterName(SubCategoryNames.BRIDGE_INSPECTIONS)}`,
-  BRIDGE_MAINTENANCE_INSTRUCTIONS: `/${getRouterName(SubCategoryNames.BRIDGE_MAINTENANCE_INSTRUCTIONS)}`,
-  TUNNELS: `/${getRouterName(SubCategoryNames.TUNNELS)}`,
-  RAILWAY_TUNNEL_RESCUE_PLANS: `/${getRouterName(SubCategoryNames.RAILWAY_TUNNEL_RESCUE_PLANS)}`,
-  SAFETY_EQUIPMENT_MAINTENANCE_INSTRUCTIONS: `/${getRouterName(
-    SubCategoryNames.SAFETY_EQUIPMENT_MAINTENANCE_INSTRUCTIONS,
+  LINE_DIAGRAMS: `/${getRouterName(MainCategoryNames.DIAGRAMS)}/${getRouterName(SubCategoryNames.LINE_DIAGRAMS)}`,
+  SPEED_DIAGRAMS: `/${getRouterName(MainCategoryNames.DIAGRAMS)}/${getRouterName(SubCategoryNames.SPEED_DIAGRAMS)}`,
+  TRACK_DIAGRAMS: `/${getRouterName(MainCategoryNames.DIAGRAMS)}/${getRouterName(SubCategoryNames.TRACK_DIAGRAMS)}`,
+  GROUPING_DIAGRAMS: `/${getRouterName(MainCategoryNames.DIAGRAMS)}/${getRouterName(
+    SubCategoryNames.GROUPING_DIAGRAMS,
   )}`,
+  INTERCHANGE_DECISIONS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(
+    SubCategoryNames.INTERCHANGE_DECISIONS,
+  )}`,
+  RAILWAY_SIGNS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(SubCategoryNames.RAILWAY_SIGNS)}`,
+  RAILWAY_ASSET_NUMBERS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(
+    SubCategoryNames.RAILWAY_ASSET_NUMBERS,
+  )}`,
+  RAILWAY_MAPS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(SubCategoryNames.RAILWAY_MAPS)}`,
+  RAILWAY_INTERCHANGE_DEVELOPMENT_NEEDS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(
+    SubCategoryNames.RAILWAY_INTERCHANGE_DEVELOPMENT_NEEDS,
+  )}`,
+  ROUTE_DOCUMENTS: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(SubCategoryNames.ROUTE_DOCUMENTS)}`,
+  RINF_REGISTER: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(SubCategoryNames.RINF_REGISTER)}`,
+  VAK_RAIL_DEPOT: `/${getRouterName(MainCategoryNames.OPERATIONS)}/${getRouterName(SubCategoryNames.VAK_RAIL_DEPOT)}`,
+  BRIDGE_INSPECTIONS: `/${getRouterName(MainCategoryNames.SPECIALTY_STRUCTURES)}/${getRouterName(
+    SubCategoryNames.BRIDGE_INSPECTIONS,
+  )}`,
+  BRIDGE_MAINTENANCE_INSTRUCTIONS: `/${getRouterName(MainCategoryNames.SPECIALTY_STRUCTURES)}/${getRouterName(
+    SubCategoryNames.BRIDGE_MAINTENANCE_INSTRUCTIONS,
+  )}`,
+  TUNNELS: `/${getRouterName(MainCategoryNames.SPECIALTY_STRUCTURES)}/${getRouterName(SubCategoryNames.TUNNELS)}`,
+  RAILWAY_TUNNEL_RESCUE_PLANS: `/${getRouterName(MainCategoryNames.SPECIALTY_STRUCTURES)}/${getRouterName(
+    SubCategoryNames.RAILWAY_TUNNEL_RESCUE_PLANS,
+  )}`,
+  SAFETY_EQUIPMENT_MAINTENANCE_INSTRUCTIONS: `/${getRouterName(MainCategoryNames.SPECIALTY_STRUCTURES)}/${getRouterName(
+    MainCategoryNames.SAFETY_EQUIPMENT,
+  )}/${getRouterName(SubCategoryNames.SAFETY_EQUIPMENT_MAINTENANCE_INSTRUCTIONS)}`,
   SAFETY_EQUIPMENT_MANUALS: `/${getRouterName(SubCategoryNames.SAFETY_EQUIPMENT_MANUALS)}`,
   INTERCHANGE_CONTACT_INFORMATION: `/${getRouterName(SubCategoryNames.INTERCHANGE_CONTACT_INFORMATION)}`,
   TRAFFIC_CONTROL_CONTACT_INFORMATION: `/${getRouterName(SubCategoryNames.TRAFFIC_CONTROL_CONTACT_INFORMATION)}`,
--- packages/frontend/src/utils/helpers.ts
+++ packages/frontend/src/utils/helpers.ts
@@ -3,6 +3,7 @@ import { SortDataType } from '../constants/Data';
 import { FileSizeUnit, LocaleLang, LocaleUnit } from '../constants/Units';
 import { Sorting } from '../contexts/SearchContext';
 import categoryData from '../assets/data/FinnishCategories.json';
+import { MainCategoryData, SubCategoryData } from '../types/types';
 
 /**
  * Generate range of years
@@ -90,36 +91,19 @@ export const getTranslatedCategoryData = (categoryData: CategoryDataParameter[])
   });
 };
 
-type SubCategoryData = {
-  LINE_DIAGRAMS: string;
-  SPEED_DIAGRAMS: string;
-  TRACK_DIAGRAMS: string;
-  GROUPING_DIAGRAMS: string;
-  INTERCHANGE_DECISIONS: string;
-  RAILWAY_SIGNS: string;
-  RAILWAY_ASSET_NUMBERS: string;
-  RAILWAY_MAPS: string;
-  RAILWAY_INTERCHANGE_DEVELOPMENT_NEEDS: string;
-  ROUTE_DOCUMENTS: string;
-  RINF_REGISTER: string;
-  VAK_RAIL_DEPOT: string;
-  BRIDGE_INSPECTIONS: string;
-  BRIDGE_MAINTENANCE_INSTRUCTIONS: string;
-  TUNNELS: string;
-  RAILWAY_TUNNEL_RESCUE_PLANS: string;
-  SAFETY_EQUIPMENT_MAINTENANCE_INSTRUCTIONS: string;
-  SAFETY_EQUIPMENT_MANUALS: string;
-  INTERCHANGE_CONTACT_INFORMATION: string;
-  TRAFFIC_CONTROL_CONTACT_INFORMATION: string;
-  MANAGEMENT_REPORTS: string;
-  MONITORING_EQUIPMENT: string;
-  REGIONAL_LIMITATIONS_DRIVER_ACTIVITY: string;
-  PLANNING_ARCHIVE: string;
-  RAILWAY_MONITORING_SERVICE: string;
+/**
+ * Iterate through category data list and return all main categories
+ * @returns
+ */
+export const getMainCategoryData = (): MainCategoryData => {
+  return categoryData.reduce((mainCategories: object, item: CategoryDataParameter) => {
+    mainCategories = { ...mainCategories, ...item.category };
+    return mainCategories;
+  }, {}) as MainCategoryData;
 };
+
 /**
  * Iterate through category data list and return all sub-categories
- * @param categoryData
  * @returns object
  */
 export const getSubCategoryData = (): SubCategoryData => {
