baed93a809d685a5e756b80063a4e8ef355628cb
NinaDang97 <khanh.2dbt@gmail.com>
2023-02-13 11:42:11 +0200

    Minor UI changes

--- packages/frontend/src/components/Notification/ErrorMessage.tsx
+++ packages/frontend/src/components/Notification/ErrorMessage.tsx
@@ -1,5 +1,6 @@
 import { Alert, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { ProtectedContainerWrapper } from '../../styles/common';
 
 type TErrorMessageProps = {
   title?: string;
@@ -11,9 +12,9 @@ export const ErrorMessage = (props: TErrorMessageProps) => {
   const { title, error } = props;
 
   return (
-    <>
+    <ProtectedContainerWrapper>
       <Typography variant="subtitle1">{title}</Typography>
       <Alert severity="error">{error?.message || t('common:error.500')}</Alert>
-    </>
+    </ProtectedContainerWrapper>
   );
 };
