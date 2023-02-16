467601330763eac666e68569a754f8b5ff3ee2a5
NinaDang97 <khanh.2dbt@gmail.com>
2023-02-16 12:56:11 +0200

    Load more category files if list over 25 items

--- packages/frontend/src/components/Files/CategoryFiles.tsx
+++ packages/frontend/src/components/Files/CategoryFiles.tsx
@@ -3,19 +3,47 @@ import { NodeItem } from './File';
 import { getRouterName } from '../../utils/helpers';
 import { ErrorMessage } from '../Notification/ErrorMessage';
 import { Spinner } from '../Spinner';
+import { useLayoutEffect, useState } from 'react';
+import { ButtonWrapper } from '../../styles/common';
+import { get } from 'lodash';
 
 type TCategoryFilesProps = {
   categoryName: string;
 };
 
 export const CategoryFiles = ({ categoryName }: TCategoryFilesProps) => {
+  const [fileList, setFileList] = useState<any[]>([]);
+  const [page, setPage] = useState(0);
   const { isLoading, isError, error, data } = useGetCategoryFiles({
     routerName: getRouterName(categoryName),
+    page: page,
   });
+
+  useLayoutEffect(() => {
+    const entries = get(data, 'list.entries', []);
+    if (entries.length) {
+      setFileList((f) => {
+        return [...f, ...data.list.entries];
+      });
+    }
+  }, [data]);
+
+  const loadMore = () => setPage(page + 1);
 
   if (isLoading) return <Spinner />;
 
   if (isError) return <ErrorMessage error={error} />;
 
-  return data.list.entries.map((node: any, index: number) => <NodeItem key={index} row={index} node={node} />);
+  return (
+    <>
+      {fileList.map((node: any, index: number) => (
+        <NodeItem key={index} row={index} node={node} />
+      ))}
+      {data.list.pagination.hasMoreItems && (
+        <ButtonWrapper color="primary" variant="outlined" onClick={loadMore}>
+          Lataa lisää
+        </ButtonWrapper>
+      )}
+    </>
+  );
 };
