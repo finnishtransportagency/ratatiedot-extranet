930aa6dbe476925940a53b7fe27caa9abecd5746
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-02-21 12:22:51 +0200

    add component to single page to test file upload

--- packages/frontend/src/pages/Others/ManagementReports/index.tsx
+++ packages/frontend/src/pages/Others/ManagementReports/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CategoryFiles } from '../../../components/Files/CategoryFiles';
 import { FileUploadDialog } from '../../../components/Files/FileUploadDialog';
 import { PageTitleWrapper, ProtectedContainerWrapper } from '../../../styles/common';
@@ -5,12 +6,15 @@ import { getSubCategoryData } from '../../../utils/helpers';
 
 export const ManagementReports = () => {
   const categoryNames = getSubCategoryData();
+  let [dialogOpen, setDialogOpen] = useState(false);
 
   return (
     <ProtectedContainerWrapper>
       <PageTitleWrapper>{categoryNames.MANAGEMENT_REPORTS}</PageTitleWrapper>
       <CategoryFiles categoryName={categoryNames.MANAGEMENT_REPORTS} />
-      <FileUploadDialog categoryName={categoryNames.MANAGEMENT_REPORTS}></FileUploadDialog>
+      {/* button to test file upload */}
+      <button onClick={() => setDialogOpen(true)}>LATAA TIEDOSTO</button>
+      {dialogOpen ? <FileUploadDialog categoryName={categoryNames.MANAGEMENT_REPORTS}></FileUploadDialog> : <></>}
     </ProtectedContainerWrapper>
   );
 };
