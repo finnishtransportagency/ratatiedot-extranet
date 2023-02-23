847ace7226c1e42f2637169e2991a5758b6603ac
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-02-23 14:41:30 +0200

    handle api responses (#162)
    * handle api responses
    
    * add reusable snackbar component

---
+++ packages/frontend/src/components/Notification/Snackbar.tsx
@@ -0,0 +1,26 @@
+import { Alert, AlertColor, Snackbar } from '@mui/material';
+import { FunctionComponent } from 'react';
+
+interface SnackBarProps {
+  text: string;
+  color: string;
+  open: boolean | undefined;
+  severity?: AlertColor;
+  onSnackbarClose?: () => void | undefined;
+}
+
+export const SnackbarAlert: FunctionComponent<SnackBarProps> = ({
+  color,
+  onSnackbarClose,
+  open,
+  text,
+  severity = 'error',
+}) => {
+  return (
+    <Snackbar open={open} onClose={onSnackbarClose} autoHideDuration={6000}>
+      <Alert sx={{ backgroundColor: color }} variant="filled" severity={severity} onClose={onSnackbarClose}>
+        {text}
+      </Alert>
+    </Snackbar>
+  );
+};
