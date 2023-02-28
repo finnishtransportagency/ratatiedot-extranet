33e211b4d5818ed77ebf74fc3d95e0c3bb9c63e4
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-02-28 11:22:25 +0200

    add jest ignore pattern for mock files

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,6 +2,7 @@
 const config = {
   verbose: true,
   transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+  testPathIgnorePatterns: ['/node_modules/', '__mocks__'],
 };
 
 export default config;
