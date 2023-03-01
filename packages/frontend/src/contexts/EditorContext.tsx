4cb177b7a0f1212d55d34e525635579c4f5b0197
NinaDang97 <khanh.2dbt@gmail.com>
2023-03-01 13:16:31 +0200

    Add EditorContext

---
+++ packages/frontend/src/contexts/EditorContext.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { createEditor } from 'slate';
+import { withReact } from 'slate-react';
+
+export const EditorContext = React.createContext({
+  editor: withReact(createEditor()),
+  value: '',
+  valueHandler: (_: any) => {},
+  valueReset: () => {},
+  kind: '',
+  kindHandler: (_: any) => {},
+});
+
+export type TSlateNode = { children: { text: string }[]; type?: string };
+const initValue: TSlateNode[] = [{ children: [{ text: '' }] }];
+
+export const EditorContextProvider = (props: any) => {
+  const [kind, setKind] = useState('');
+  const [editor, setEditor] = useState(() => withReact(createEditor()));
+  const [value, setValue] = useState(JSON.stringify(initValue));
+
+  const kindHandler = (k: string) => setKind(k);
+
+  const valueHandler = (v: any) => setValue(v);
+
+  const valueReset = () => {
+    setValue(JSON.stringify(initValue));
+    setEditor(() => withReact(createEditor()));
+  };
+
+  return (
+    <EditorContext.Provider
+      value={{
+        editor: editor,
+        value: value,
+        valueHandler: valueHandler,
+        valueReset: valueReset,
+        kind: kind,
+        kindHandler: kindHandler,
+      }}
+    >
+      {props.children}
+    </EditorContext.Provider>
+  );
+};
