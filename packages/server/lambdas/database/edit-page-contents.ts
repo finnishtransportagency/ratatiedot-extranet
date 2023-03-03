59d1160e9c4529edb887068c04281d7014c56c57
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-03-03 11:00:44 +0200

    feat/RTENU-138-content-edit (#171)
    * content update api endpoint and lambda function
    
    * remove unused imports

--- packages/server/lambdas/database/edit-page-contents.ts
+++ packages/server/lambdas/database/edit-page-contents.ts
@@ -1,44 +1,46 @@
 import { CategoryDataBase } from '@prisma/client';
 import { ALBEvent, ALBResult } from 'aws-lambda';
-import { isEmpty } from 'lodash';
 import { findEndpoint } from '../../utils/alfresco';
 
 import { getRataExtraLambdaError, RataExtraLambdaError } from '../../utils/errors';
 import { log } from '../../utils/logger';
 import { getUser, validateReadUser, validateWriteUser } from '../../utils/userService';
 import { DatabaseClient } from './client';
+import { Prisma } from '@prisma/client';
+import { isEmpty } from 'lodash';
+import { handlePrismaError, PrismaError } from './error/databaseError';
 
 const database = await DatabaseClient.build();
 
 let fileEndpointsCache: Array<CategoryDataBase> = [];
 
 /**
- * Update custom content for page. Example request: /api/database/page-contents/linjakaaviot
+ * Edit page content. Example request: /api/database/page-contents/linjakaaviot
  * @param {ALBEvent} event
- * @param {{string}} event.path Path should end with the page to update the custom content for
- * @param {{Record<string, string>}} event.body Page to get the custom content for
- * @returns  {Promise<ALBResult>} JSON stringified object of updated contents inside body
+ * @param {{string}} event.path Path should end with the page to get the custom content for
+ * @returns  {Promise<ALBResult>} JSON stringified object of contents inside body
  */
 export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
   try {
     const paths = event.path.split('/');
     const category = paths.pop();
-    const body: Record<string, string> = event.body ? JSON.parse(event.body) : {};
-    log.debug(`Request body: ${JSON.stringify(body)}`);
 
     const user = await getUser(event);
-    log.info(user, `Updating page contents for page ${category}`);
+    log.info(user, `Updating page contents: ${category}`);
     validateReadUser(user);
+
+    if (isEmpty(event.body) || event.body === null) {
+      throw new RataExtraLambdaError('Request body missing', 400);
+    }
 
     if (!category || paths.pop() !== 'page-contents') {
       throw new RataExtraLambdaError('Category missing from path', 400);
     }
-    if (isEmpty(body)) {
-      throw new RataExtraLambdaError('Request body missing', 400);
-    }
     if (!fileEndpointsCache.length) {
+      log.debug('Cache empty');
       fileEndpointsCache = await database.categoryDataBase.findMany();
     }
+    log.debug(`Cached ${JSON.stringify(fileEndpointsCache)}`);
     const categoryData = findEndpoint(category, fileEndpointsCache);
     if (!categoryData) {
       throw new RataExtraLambdaError('Category not found', 404);
@@ -47,17 +49,33 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
     const writeRole = categoryData.writeRights;
     validateWriteUser(user, writeRole);
 
-    const contents = await database.categoryDataContents.update({
-      where: { baseId: categoryData.id },
-      data: { fields: body },
+    const whereClause = Prisma.validator<Prisma.CategoryDataContentsWhereInput>()({
+      baseId: categoryData.id,
     });
+
+    const dataClause = Prisma.validator<Prisma.CategoryDataContentsUpdateInput>()({
+      fields: JSON.parse(event.body),
+    });
+
+    const updateContent = async () => {
+      try {
+        return await database.categoryDataContents.update({
+          where: whereClause,
+          data: dataClause,
+        });
+      } catch (error) {
+        handlePrismaError(error as PrismaError);
+      }
+    };
+
+    const updatedContent = await updateContent();
 
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ fields: contents?.fields }),
+      body: JSON.stringify({ fields: updatedContent?.fields }),
     };
   } catch (err) {
     log.error(err);
