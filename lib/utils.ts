0f888a0c77d9d6e689512a69968cf7c63a1308c5
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-03-06 12:59:03 +0200

    use isProductionStack functio to check environment

--- lib/utils.ts
+++ lib/utils.ts
@@ -30,11 +30,6 @@ export const isFeatOrLocalStack = (rataExtraEnv: RataExtraEnvironment) =>
 export const isLocalStack = (rataExtraEnv: RataExtraEnvironment) => rataExtraEnv === ENVIRONMENTS.local;
 
 /**
- * Returns true if environment is set as prod
- */
-export const isProductionEnvirEnvironment = (rataExtraEnv: string) => rataExtraEnv === ENVIRONMENTS.prod;
-
-/**
  * Returns whether the stack is one of the two permanent RataExtra stacks
  * - development main stack that corresponds to development main branch in Github
  * - producition stack that corresponds to production branch in Github
@@ -42,14 +37,19 @@ export const isProductionEnvirEnvironment = (rataExtraEnv: string) => rataExtraE
 export const isPermanentStack = (stackId: string, rataExtraEnv: RataExtraEnvironment) =>
   isDevelopmentMainStack(stackId, rataExtraEnv) || isProductionStack(stackId, rataExtraEnv);
 
-export const getVpcAttributes = (rataExtraEnv: RataExtraEnvironment) => {
-  const vpcId = isProductionEnvirEnvironment(rataExtraEnv) ? 'vpc-0f7ce3c168bc01755' : 'vpc-092f1d064d39ca6a1';
+export const getVpcAttributes = (stackId: string, rataExtraEnv: RataExtraEnvironment) => {
+  let vpcId = 'vpc-0f7ce3c168bc01755';
+  let privateSubnetIds = ['subnet-01c9e778cbb6cb767', 'subnet-03598efd60ee4b545'];
+  if (!isProductionStack(stackId, rataExtraEnv)) {
+    vpcId = 'vpc-092f1d064d39ca6a1';
+    privateSubnetIds = ['subnet-05ba766fa5c0f0eb0', 'subnet-019cb289645adae50'];
+  }
   return {
     vpcId: vpcId,
     availabilityZones: ['eu-west-1a', 'eu-west-1b'],
-    privateSubnetIds: ['subnet-05ba766fa5c0f0eb0', 'subnet-019cb289645adae50'],
+    privateSubnetIds: privateSubnetIds,
   };
 };
 
-export const getSecurityGroupId = (rataExtraEnv: RataExtraEnvironment) =>
-  isProductionEnvirEnvironment(rataExtraEnv) ? 'sg-0fa9cd225ca77951a' : 'sg-04a38c9d8b10a6bbd';
+export const getSecurityGroupId = (stackId: string, rataExtraEnv: RataExtraEnvironment) =>
+  isProductionStack(stackId, rataExtraEnv) ? 'sg-0fa9cd225ca77951a' : 'sg-04a38c9d8b10a6bbd';
