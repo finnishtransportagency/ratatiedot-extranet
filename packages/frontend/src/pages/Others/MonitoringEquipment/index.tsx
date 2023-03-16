da67fca38eddc2d672354866b0d7941737dc215f
NinaDang97 <31354481+NinaDang97@users.noreply.github.com>
2023-03-16 09:57:06 +0200

    Move <CategoryFiles /> to every protected category pages (#186)

--- packages/frontend/src/pages/Others/MonitoringEquipment/index.tsx
+++ packages/frontend/src/pages/Others/MonitoringEquipment/index.tsx
@@ -1,4 +1,3 @@
-import { CategoryFiles } from '../../../components/Files/CategoryFiles';
 import { PageTitleWrapper, ProtectedContainerWrapper } from '../../../styles/common';
 import { getSubCategoryData } from '../../../utils/helpers';
 
@@ -8,7 +7,6 @@ export const MonitoringEquipment = () => {
   return (
     <ProtectedContainerWrapper>
       <PageTitleWrapper>{categoryNames.MONITORING_EQUIPMENT}</PageTitleWrapper>
-      <CategoryFiles categoryName={categoryNames.MONITORING_EQUIPMENT} />
     </ProtectedContainerWrapper>
   );
 };
