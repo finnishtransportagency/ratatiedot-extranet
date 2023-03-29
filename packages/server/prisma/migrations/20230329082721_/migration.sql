0c63cdc2a957953aa459e7410a7d4d9125e44de7
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-03-29 11:28:21 +0300

    migrate migration files :)

--- packages/server/prisma/migrations/20230329082721_/migration.sql
+++ packages/server/prisma/migrations/20230329082721_/migration.sql
@@ -3,6 +3,7 @@
 
   - The primary key for the `CategoryDataBase` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - The primary key for the `CategoryDataContents` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[alfrescoFolder]` on the table `CategoryDataBase` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- DropForeignKey
@@ -26,37 +27,49 @@ DROP SEQUENCE "CategoryDataContents_id_seq";
 -- CreateTable
 CREATE TABLE "CategoryComponent" (
     "id" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
 
     CONSTRAINT "CategoryComponent_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "NodeList" (
+CREATE TABLE "Node" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "nodeId" TEXT NOT NULL,
-    "categoryComponentId" TEXT,
+    "categoryComponentId" TEXT NOT NULL,
 
-    CONSTRAINT "NodeList_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Node_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Card" (
     "id" TEXT NOT NULL,
-    "categoryComponentId" TEXT,
+    "title" TEXT NOT NULL,
+    "content" JSONB NOT NULL,
+    "categoryComponentId" TEXT NOT NULL,
 
     CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Node_categoryComponentId_key" ON "Node"("categoryComponentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Card_categoryComponentId_key" ON "Card"("categoryComponentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CategoryDataBase_alfrescoFolder_key" ON "CategoryDataBase"("alfrescoFolder");
 
 -- AddForeignKey
 ALTER TABLE "CategoryDataContents" ADD CONSTRAINT "CategoryDataContents_baseId_fkey" FOREIGN KEY ("baseId") REFERENCES "CategoryDataBase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CategoryComponent" ADD CONSTRAINT "CategoryComponent_id_fkey" FOREIGN KEY ("id") REFERENCES "CategoryDataBase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CategoryComponent" ADD CONSTRAINT "CategoryComponent_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "CategoryDataBase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "NodeList" ADD CONSTRAINT "NodeList_categoryComponentId_fkey" FOREIGN KEY ("categoryComponentId") REFERENCES "CategoryComponent"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Node" ADD CONSTRAINT "Node_categoryComponentId_fkey" FOREIGN KEY ("categoryComponentId") REFERENCES "CategoryComponent"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Card" ADD CONSTRAINT "Card_categoryComponentId_fkey" FOREIGN KEY ("categoryComponentId") REFERENCES "CategoryComponent"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Card" ADD CONSTRAINT "Card_categoryComponentId_fkey" FOREIGN KEY ("categoryComponentId") REFERENCES "CategoryComponent"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
