ec4ae1864049c1a26db2486f3bd4109f113267c5
NinaDang97 <khanh.2dbt@gmail.com>
2023-05-03 14:47:22 +0300

    Add buttons to add/remove page from favorite list

--- packages/frontend/src/pages/ProtectedPage/index.tsx
+++ packages/frontend/src/pages/ProtectedPage/index.tsx
@@ -1,4 +1,8 @@
 import React, { useContext } from 'react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { ContentWrapper, ContainerWrapper } from './index.styles';
 import { NavBar } from '../../components/NavBar';
@@ -11,6 +15,9 @@ import { FileUploadDialogButton } from '../../components/Files/FileUploadDialogB
 import { useLocation } from 'react-router-dom';
 import { CategoryFiles } from '../../components/Files/CategoryFiles';
 import { getCategoryRouteName } from '../../routes';
+import { ButtonWrapper } from '../../styles/common';
+import { useTranslation } from 'react-i18next';
+import { IMenuItem, MenuContext } from '../../contexts/MenuContext';
 
 type Props = {
   children: React.ReactElement;
@@ -19,21 +26,61 @@ type Props = {
 // Protected routes will be wrapped around by ProtectedPage
 // to get access navigation bar and title bar
 export const ProtectedPage = ({ children }: Props) => {
+  const { t } = useTranslation(['common']);
   const { openEdit, openToolbar } = useContext(AppBarContext);
   const { value } = useContext(EditorContext);
+  const { favoriteCategories, addFavoriteHandler, removeFavoriteHandler } = useContext(MenuContext);
   const location = useLocation();
   const categoryRouteName = getCategoryRouteName(location);
 
   const isEditorOpened = openToolbar || (openEdit && !isSlateValueEmpty(value)) || !isSlateValueEmpty(value);
+
+  const isFavorite = favoriteCategories.some((c: IMenuItem) => {
+    return c.to?.indexOf(categoryRouteName) !== -1;
+  });
+
+  const AddFavoriteButton = () => {
+    return (
+      <ButtonWrapper sx={{ textTransform: 'none', padding: 0 }} onClick={() => addFavoriteHandler(categoryRouteName)}>
+        <AddCircleOutlineIcon fontSize="small" />
+        {t('common:action.add_favorite')}
+      </ButtonWrapper>
+    );
+  };
+
+  const RemoveFavoriteButton = () => {
+    return (
+      <ButtonWrapper
+        sx={{ textTransform: 'none', padding: 0 }}
+        onClick={() => removeFavoriteHandler(categoryRouteName)}
+      >
+        <RemoveCircleOutlineIcon fontSize="small" />
+        {t('common:action.remove_favorite')}
+      </ButtonWrapper>
+    );
+  };
 
   return (
     <ContainerWrapper>
       <NavBar />
       <ContentWrapper openedit={openEdit} opentoolbar={openToolbar}>
         {isEditorOpened && <FileUploadDialogButton categoryName={categoryRouteName} />}
+        {categoryRouteName ? isFavorite ? <RemoveFavoriteButton /> : <AddFavoriteButton /> : <></>}
         {children}
         {isEditorOpened && <SlateInputField />}
         {categoryRouteName && <CategoryFiles />}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
         <Footer />
       </ContentWrapper>
     </ContainerWrapper>
