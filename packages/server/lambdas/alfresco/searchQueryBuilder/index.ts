9f7066700781c6f7cee1e449fa60f3acec275e94
NinaDang97 <khanh.2dbt@gmail.com>
2023-05-08 18:00:15 +0300

    Get parameter value. Logging

--- packages/server/lambdas/alfresco/searchQueryBuilder/index.ts
+++ packages/server/lambdas/alfresco/searchQueryBuilder/index.ts
@@ -1,19 +1,23 @@
 import { log } from '../../../utils/logger';
 import { LuceneQueryBuilder } from './luceneQueryBuilder';
 import { QueryLanguage, Query, QueryRequest } from './types';
+import { getParameter } from '../../../utils/parameterStore';
 
 const alfrescoSitePathName = process.env.SSM_ALFRESCO_SITE_PATH || '';
 
-export const searchQueryBuilder = ({
+export const searchQueryBuilder = async ({
   searchParameters,
   page = 0,
   language = QueryLanguage.LUCENE,
   additionalFields,
   sort = [],
-}: QueryRequest): Query => {
+}: QueryRequest): Promise<Query> => {
   switch (language) {
     case QueryLanguage.LUCENE:
-      const luceneQueryBuilder = new LuceneQueryBuilder(alfrescoSitePathName);
+      const alfrescoSitePath = await getParameter(alfrescoSitePathName);
+      const luceneQueryBuilder = new LuceneQueryBuilder(alfrescoSitePath);
+      log.info('alfrescoSitePathName: ', alfrescoSitePathName);
+      log.info('alfrescoSitePath: ', alfrescoSitePath);
       return {
         query: {
           query: luceneQueryBuilder.queryBuilder(searchParameters),
