edf783376093d24d1e80fc0812bde3f5e4f0efd1
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-05-11 19:38:54 +0300

    WIP

--- packages/server/lambdas/alfresco/delete-folder.ts
+++ packages/server/lambdas/alfresco/delete-folder.ts
@@ -19,8 +19,9 @@ const deleteFolder = async (options: RequestInit, nodeId: string) => {
   const url = `${alfrescoCoreAPIUrl}/nodes/${nodeId}`;
   try {
     const res = await fetch(url, options);
-    devLog.debug('deleted fetch response: ' + res);
-    return res;
+    const response = await res.json();
+    devLog.debug('deleted fetch response: ' + response);
+    return response;
   } catch (err) {
     console.error('error:' + err);
   }
