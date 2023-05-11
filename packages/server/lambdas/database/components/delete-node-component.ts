6afa80d1ebd1afc90ec5cdab7067f2aad5df1cec
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-05-11 16:49:03 +0300

    WIP

--- packages/server/lambdas/database/components/delete-node-component.ts
+++ packages/server/lambdas/database/components/delete-node-component.ts
@@ -3,12 +3,24 @@ import { handlePrismaError, PrismaError } from '../error/databaseError';
 
 const database = await DatabaseClient.build();
 
-export const deleteComponent = async (componentId: string) => {
+export const deleteComponent = async (alfrescoId: string) => {
   let response = null;
   try {
+    const component = await database.categoryComponent.findFirst({
+      where: {
+        node: {
+          alfrescoNodeId: alfrescoId,
+        },
+      },
+    });
+
+    if (!component) {
+      return;
+    }
+
     response = await database.categoryComponent.delete({
       where: {
-        id: componentId,
+        id: component?.id,
       },
     });
   } catch (error) {
