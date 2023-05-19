1dd97dec22cf8165f5498802b18603f2b38b902a
NinaDang97 <khanh.2dbt@gmail.com>
2023-05-19 14:44:38 +0300

    Get folder id as query parameter instead of folder name

--- packages/server/lambdas/alfresco/list-files.ts
+++ packages/server/lambdas/alfresco/list-files.ts
@@ -10,6 +10,7 @@ import { DatabaseClient } from '../database/client';
 import { searchQueryBuilder } from './searchQueryBuilder';
 import {
   AdditionalFields,
+  IAncestorSearchParameter,
   IFolderSearchParameter,
   IParentSearchParameter,
   QueryLanguage,
@@ -31,13 +32,7 @@ export type TNode = {
   };
 };
 
-const searchByTermWithParent = async (
-  uid: string,
-  alfrescoParent: string,
-  alfrescoFolder = '',
-  page: number,
-  language: QueryLanguage,
-) => {
+const searchByTermWithParent = async (uid: string, alfrescoParent: string, page: number, language: QueryLanguage) => {
   try {
     const searchParameters = [];
     const parent: IParentSearchParameter = {
@@ -45,14 +40,6 @@ const searchByTermWithParent = async (
       parent: alfrescoParent,
     };
     searchParameters.push(parent);
-    if (alfrescoFolder) {
-      const folder: IFolderSearchParameter = {
-        parameterName: SearchParameterName.FOLDER,
-        name: alfrescoFolder,
-      };
-      searchParameters.push(folder);
-    }
-
     const bodyRequest = searchQueryBuilder({
       searchParameters: searchParameters,
       page: page,
@@ -70,12 +57,30 @@ const searchByTermWithParent = async (
   }
 };
 
+const getFolder = async (id: string) => {
+  try {
+    const alfrescoCoreAPIUrl = `${getAlfrescoUrlBase()}/alfresco/versions/1`;
+    const response = await axios.get(`${alfrescoCoreAPIUrl}/nodes/${id}?where=(isFolder=true)&include=path`);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const isFolderInCategory = async (folderPath: string, category: string) => {
+  // Split the path into its components
+  const pathComponents = folderPath.split('/');
+
+  // Check if the parent folder name is among the path components
+  return pathComponents.includes(category);
+};
+
 const database = await DatabaseClient.build();
 
 let fileEndpointsCache: Array<CategoryDataBase> = [];
 
 /**
- * Get the list of files embedded to given page. Example: /api/alfresco/files?category=linjakaaviot&subcategory=kansio_nimi
+ * Get the list of files embedded to given page. Example: /api/alfresco/files?category=linjakaaviot&folderid=123
  * @param {ALBEvent} event
  * @param {{category: string, page?: number, language?: QueryLanguage }} event.queryStringParameters
  * @param {string} event.queryStringParameters.category Page to be searched for
@@ -86,15 +91,31 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
     const user = await getUser(event);
     const params = event.queryStringParameters;
     const category = params?.category;
-    const subCategory = params?.subcategory ? decodeURI(params?.subcategory) : params?.subcategory;
-    log.info(user, `Fetching files for for page ${category} with folder ${subCategory} `);
+    const folderId = params?.folderid;
+    log.info(user, `Fetching files for for page ${category} with folder ${folderId} `);
+    const responseBody = {
+      hasClassifiedContent: true,
+      data: {
+        list: {
+          pagination: {
+            count: 0,
+            hasMoreItems: false,
+            totalItems: 0,
+            skipCount: 0,
+            maxItems: 25,
+          },
+          context: {},
+          entries: [],
+        },
+      },
+    }; // Default response
 
     validateReadUser(user);
     if (!category) {
       throw new RataExtraLambdaError('Category missing', 400);
     }
     const page = params?.page ? parseInt(params?.page) : 0;
-    const categoryPage = subCategory ? 0 : page;
+    const categoryPage = folderId ? 0 : page;
     const language = (params?.language as QueryLanguage) ?? QueryLanguage.LUCENE;
     if (!Object.values(QueryLanguage).includes(language)) {
       throw new RataExtraLambdaError('Invalid language', 400);
@@ -104,23 +125,20 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
     }
     const endpoint = findEndpoint(category, fileEndpointsCache);
     const alfrescoParent = endpoint?.alfrescoFolder;
-    const hasClassifiedContent = endpoint?.hasClassifiedContent;
+    responseBody.hasClassifiedContent = endpoint?.hasClassifiedContent || true;
 
     if (!alfrescoParent) {
       throw new RataExtraLambdaError('Category not found', 404);
     }
-
-    const categoryData = await searchByTermWithParent(user.uid, alfrescoParent, subCategory, categoryPage, language);
-    if (subCategory) {
-      const folderId = get(categoryData, 'list.entries[0].entry.id', -1);
-      const subCategoryData = await searchByTermWithParent(user.uid, folderId, '', page, language);
-      return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ data: subCategoryData, hasClassifiedContent }),
-      };
+    if (!folderId) {
+      responseBody.data = await searchByTermWithParent(user.uid, alfrescoParent, page, language);
+    } else {
+      const foundFolder = await getFolder(folderId);
+      const folderPath = get(foundFolder, 'entry.path.name', '');
+      const isFolderDescendantOfCategory = await isFolderInCategory(folderPath, category);
+      if (isFolderDescendantOfCategory) {
+        responseBody.data = await searchByTermWithParent(user.uid, folderId, categoryPage, language);
+      }
     }
 
     return {
@@ -128,7 +146,7 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ data: categoryData, hasClassifiedContent }),
+      body: JSON.stringify(responseBody),
     };
   } catch (err) {
     log.error(err);
