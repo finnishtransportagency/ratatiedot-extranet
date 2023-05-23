e03b3cc6cb205750395600f6fbe3c63f8f7df474
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2023-05-23 07:37:55 +0300

    infra/RTENU-155: One last test in this branch

--- lib/rataextra-pipeline.ts
+++ lib/rataextra-pipeline.ts
@@ -79,37 +79,35 @@ export class RataExtraPipelineStack extends Stack {
 
     // TOOD: Only run on main
 
-    if (isDevelopmentMainStack(config.stackId, config.env)) {
-      const sonarQube = new CodeBuildStep('Scan', {
-        input: github,
-        buildEnvironment: {
-          environmentVariables: {
-            SONAR_TOKEN: {
-              value: config.sonarQubeToken,
-              type: BuildEnvironmentVariableType.SECRETS_MANAGER,
-            },
+    const sonarQube = new CodeBuildStep('Scan', {
+      input: github,
+      buildEnvironment: {
+        environmentVariables: {
+          SONAR_TOKEN: {
+            value: config.sonarQubeToken,
+            type: BuildEnvironmentVariableType.SECRETS_MANAGER,
           },
         },
-        installCommands: [
-          'export SONAR_SCANNER_VERSION=4.7.0.2747',
-          'export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux',
-          'curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip',
-          'unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/',
-          'export PATH=$SONAR_SCANNER_HOME/bin:$PATH',
-          'export SONAR_SCANNER_OPTS="-server"',
-        ],
-        commands: [
-          `sonar-scanner \
+      },
+      installCommands: [
+        'export SONAR_SCANNER_VERSION=4.7.0.2747',
+        'export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux',
+        'curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip',
+        'unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/',
+        'export PATH=$SONAR_SCANNER_HOME/bin:$PATH',
+        'export SONAR_SCANNER_OPTS="-server"',
+      ],
+      commands: [
+        `sonar-scanner \
             -Dsonar.projectKey=Ratatieto \
             -Dsonar.sources=. \
             -Dsonar.host.url=${sonarQubeUrl}`,
-        ],
-      });
+      ],
+    });
 
-      pipeline.addWave('SonarQube', {
-        pre: [sonarQube],
-      });
-    }
+    pipeline.addWave('SonarQube', {
+      pre: [sonarQube],
+    });
 
     pipeline.addStage(
       new RataExtraApplication(this, 'RataExtra', {
