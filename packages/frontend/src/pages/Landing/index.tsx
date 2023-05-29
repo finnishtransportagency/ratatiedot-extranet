3a504f7e5bf425ef49306b6bc5efca37a2684a4f
NinaDang97 <31354481+NinaDang97@users.noreply.github.com>
2023-05-29 15:20:40 +0300

    Add contact info to frontpage (#257)

--- packages/frontend/src/pages/Landing/index.tsx
+++ packages/frontend/src/pages/Landing/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 
 import { SubtitleWrapper, ParagraphWrapper } from './index.styles';
 import { ProtectedContainerWrapper } from '../../styles/common';
+import { Link } from '@mui/material';
 
 export const Landing = () => {
   const { t } = useTranslation(['common', 'landing']);
@@ -11,6 +12,18 @@ export const Landing = () => {
       <SubtitleWrapper variant="subtitle1">{t('landing:welcome.text')}</SubtitleWrapper>
       <ParagraphWrapper variant="body1">{t('landing:welcome.description_primary')}</ParagraphWrapper>
       <ParagraphWrapper variant="body1">{t('landing:welcome.description_secondary')}</ParagraphWrapper>
+      <ParagraphWrapper variant="body1">
+        {t('landing:welcome.contact')}
+        <Link
+          href="mailto:ratatieto@vayla.fi"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ display: 'inline', textDecoration: 'none' }}
+        >
+          ratatieto@vayla.fi
+        </Link>
+        .
+      </ParagraphWrapper>
     </ProtectedContainerWrapper>
   );
 };
