38b295e757ec0943f70a73e4c53f7e1ba521baaf
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-06-05 14:55:01 +0300

    test

--- packages/server/utils/parser.ts
+++ packages/server/utils/parser.ts
@@ -1,26 +1,43 @@
 import { ALBEventHeaders } from 'aws-lambda';
 import busboy, { FileInfo } from 'busboy';
 import { Readable } from 'stream';
+import { devLog } from './logger';
 
 export interface ParsedFormDataOptions {
   [key: string]: string | Buffer | Readable | FileInfo;
 }
 
+interface FormData {
+  fieldname: string;
+  fileinfo?: FileInfo;
+  file: any;
+}
+
 export const parseForm = (buffer: Buffer | string, headers: ALBEventHeaders) => {
   return new Promise<ParsedFormDataOptions>((resolve, reject) => {
-    const form = {} as ParsedFormDataOptions;
-    const bb = busboy({ headers: headers });
+    const bb = busboy({
+      headers: {
+        ...headers,
+        'content-type': headers['Content-Type'] || headers['content-type'],
+      },
+    });
+    let form = {} as ParsedFormDataOptions;
 
     bb.on('file', (fieldname: string, file: Readable, fileinfo: FileInfo) => {
-      form.fieldname = fieldname;
-      form.file = file;
-      form.fileinfo = fileinfo as FileInfo;
-
+      const temp: FormData = { file: [], fieldname: '' };
       file.on('data', (data: Buffer) => {
-        form[fieldname] = data;
+        temp.file.push(data);
       });
 
-      file.on('end', () => console.log('File parse finished'));
+      file.on('end', () => {
+        temp.file = Buffer.concat(temp.file);
+        temp.fieldname = fieldname;
+        temp.fileinfo = fileinfo as FileInfo;
+
+        form = { ...temp };
+        devLog.debug('form: ' + form);
+        console.log('File parse finished');
+      });
     });
 
     bb.on('finish', () => {
