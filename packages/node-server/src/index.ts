fcabcb92f70a2f60dfeff4a5e347c8daef43ade0
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-06-12 12:36:08 +0300

    wip

--- packages/node-server/src/index.ts
+++ packages/node-server/src/index.ts
@@ -22,14 +22,7 @@ function uploadFiles(req) {
   console.log(req);
 }
 
-/**
- * Upload custom content for page. Example request: /api/alfresco/file/linjakaaviot
- * @param {FileUploadRequest} event
- * @param {{string}} event.path Path should end with the page to upload the file to
- * @param {{string}} event.body File contents and metadata to upload
- * @returns  {Promise<ALBResult>} JSON stringified object of uploaded file metadata
- */
-async function handleRequest(event: any): Promise<any> {
+const handleRequest = async (event: any): Promise<any> => {
   try {
     const paths = event.path.split('/');
     const category = paths.pop();
@@ -43,12 +36,12 @@ async function handleRequest(event: any): Promise<any> {
   } catch (err) {
     return err;
   }
-}
+};
 
 const app = express();
 const port = 3000;
 
-const uploadFile = (req, res) => {
+const uploadFile = (req: Request, res: Response) => {
   console.log(req.body);
   console.log(req.file);
   res.json({ message: 'Successfully uploaded files' });
