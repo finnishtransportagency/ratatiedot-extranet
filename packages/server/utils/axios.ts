cf037e334383bc16d1d9a4a9b3fd5ed80575464c
noravux <tukiainen.jiri@gmail.com>
2023-06-15 12:16:58 +0300

    wip

--- packages/server/utils/axios.ts
+++ packages/server/utils/axios.ts
@@ -12,9 +12,12 @@ alfrescoAxios.interceptors.response.use(
   (error) => {
     if (axios.isAxiosError(error)) {
       const listFilesEndpoint = /\/api\/nodes\/[A-Za-z0-9-]+\/where=\(isFolder=true\)&include=path/;
+      console.log('url and regex match: ', error.config?.url && listFilesEndpoint.test(error.config?.url));
       if (error.config?.url && listFilesEndpoint.test(error.config?.url)) {
+        console.log('list-files endpoint, check for 404');
         // In case nodeId doesn't exist, Alfresco throws 404
         if (error.response?.status === 404) {
+          console.log('404, return null');
           return null;
         }
       }
@@ -23,6 +26,7 @@ alfrescoAxios.interceptors.response.use(
         message: error.message,
         stack: error.stack,
       };
+      console.log('throw simplifiedError');
       throw simplifiedError;
     }
     throw error;
