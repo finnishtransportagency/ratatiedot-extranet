7b8f51c2bcadc26b1a6a69bee0be9631a80c5d1f
noravux <tukiainen.jiri@gmail.com>
2023-06-15 12:56:09 +0300

    wip

--- packages/server/utils/axios.ts
+++ packages/server/utils/axios.ts
@@ -11,13 +11,13 @@ alfrescoAxios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (axios.isAxiosError(error)) {
-      const listFilesEndpoint = /\/api\/nodes\/[A-Za-z0-9-]+\/where=\(isFolder=true\)&include=path/;
+      const listFilesEndpoint = /\/alfresco\/versions\/1\/nodes\/[A-Za-z0-9-]+\?where=\(isFolder=true\)&include=path/;
       console.log('url: ', error.config?.url);
 
       if (error.config?.url) {
         console.log('regex test: ', listFilesEndpoint.test(error.config?.url));
       }
-      if (error.config?.url !== undefined && listFilesEndpoint.test(error.config?.url)) {
+      if (error.config?.url && listFilesEndpoint.test(error.config?.url)) {
         console.log('list-files endpoint, check for 404');
         // In case nodeId doesn't exist, Alfresco throws 404
         if (error.response?.status === 404) {
