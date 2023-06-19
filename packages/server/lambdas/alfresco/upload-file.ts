3e604d0a09b5b7565e3deacd42d7105f44a17ab0
noravux <tukiainen.jiri@gmail.com>
2023-06-19 15:13:50 +0300

    wip

--- packages/server/lambdas/alfresco/upload-file.ts
+++ packages/server/lambdas/alfresco/upload-file.ts
@@ -93,6 +93,7 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult | undefi
 
     const headers = (await getAlfrescoOptions(user.uid)).headers;
     const requestOptions = (await fileRequestBuilder(event, headers)) as AxiosRequestOptions;
+    console.log('requestOptions: ', requestOptions);
     const result = await postFile(requestOptions, targetNode);
 
     auditLog.info(
