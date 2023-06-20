a770ec67b300423157217b3902174589b2984c16
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-06-20 18:46:56 +0300

    wip

--- lib/rataextra-node-backend.ts
+++ lib/rataextra-node-backend.ts
@@ -9,6 +9,7 @@ import {
   InitSource,
   UserData,
   InitCommand,
+  InitConfig,
 } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { ManagedPolicy, ServicePrincipal, Role } from 'aws-cdk-lib/aws-iam';
@@ -51,11 +52,20 @@ export class RatatietoNodeBackendConstruct extends Construct {
       managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
     });
 
-    // InitCommand.shellCommand('cd /source/packages/node-server && npm ci && npm run build && npm run start'),
-    const init = CloudFormationInit.fromElements(
-      InitSource.fromGitHub('/source', 'finnishtransportagency', 'ratatiedot-extranet', config.branch),
-      InitCommand.shellCommand('echo Hello world!'),
-    );
+    const init = CloudFormationInit.fromConfigSets({
+      configSets: {
+        // Applies the configs below in this order
+        default: ['getSource', 'nodeBuild'],
+      },
+      configs: {
+        getSource: new InitConfig([
+          InitSource.fromGitHub('/source', 'finnishtransportagency', 'ratatiedot-extranet', config.branch),
+        ]),
+        nodeBuild: new InitConfig([
+          InitCommand.shellCommand('cd /source/packages/node-server && npm ci && npm run build && npm run start'),
+        ]),
+      },
+    });
 
     const autoScalingGroup = new AutoScalingGroup(this, 'AutoScalingGroup', {
       vpc,
