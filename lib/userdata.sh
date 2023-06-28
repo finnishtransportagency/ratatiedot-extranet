ac82012f90119336ebbfa4f7e74ee085c67a5a87
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-06-28 14:48:43 +0300

    wip

--- lib/userdata.sh
+++ lib/userdata.sh
@@ -12,11 +12,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-cat <<EOF >> /home/ec2-user/.bashrc
-  export NVM_DIR="/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-EOF
-
 nvm install v16.20.0
 nvm use v16.20.0
 nvm -v
