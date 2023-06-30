c54be0bddd37074ee85751d6fc7bd9929299a7bd
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-06-30 13:11:42 +0300

    wip

--- bastion-feat-backend-pipe.sh
+++ bastion-feat-backend-pipe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./loadbastionenv
-aws ssm start-session --target $INSTANCE_ID --profile $PROFILE --region eu-west-1 --document-name AWS-StartPortForwardingSession --parameters '{"portNumber":["84"],"localPortNumber":["3002"]}'
+aws ssm start-session --target $INSTANCE_ID --profile $PROFILE --region eu-west-1 --document-name AWS-StartPortForwardingSession --parameters '{"portNumber":["88"],"localPortNumber":["3002"]}'
