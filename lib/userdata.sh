0a96ef7dbe5a8d47a937641923ca23027f378d2d
noravux <tukiainen.jiri@gmail.com>
2023-07-31 12:15:51 +0300

    wip

--- lib/userdata.sh
+++ lib/userdata.sh
@@ -5,11 +5,11 @@ if ! [ -d "/var/log/nodeserver" ]; then
   echo "Creating /var/log/nodeserver/"
   mkdir /var/log/nodeserver/
 fi
-exec > /var/log/nodeserver/logs.log 2>&1
-export "ENVIRONMENT={rataExtraEnv}" "SSM_DATABASE_NAME_ID={SSM_DATABASE_NAME}" SSM_DATABASE_DOMAIN_ID="{SSM_DATABASE_DOMAIN}" "SSM_DATABASE_PASSWORD_ID={SSM_DATABASE_PASSWORD}" "ALFRESCO_API_KEY_NAME={alfrescoAPIKey}" "ALFRESCO_API_URL={alfrescoAPIUrl}" "ALFRESCO_API_ANCESTOR={alfrescoAncestor}" "JWT_TOKEN_ISSUER={jwtTokenIssuer}" "MOCK_UID={mockUid}"
+#exec > /var/log/nodeserver/logs.log 2>&1
+#export "ENVIRONMENT={rataExtraEnv}" "SSM_DATABASE_NAME_ID={SSM_DATABASE_NAME}" SSM_DATABASE_DOMAIN_ID="{SSM_DATABASE_DOMAIN}" "SSM_DATABASE_PASSWORD_ID={SSM_DATABASE_PASSWORD}" "ALFRESCO_API_KEY_NAME={alfrescoAPIKey}" "ALFRESCO_API_URL={alfrescoAPIUrl}" "ALFRESCO_API_ANCESTOR={alfrescoAncestor}" "JWT_TOKEN_ISSUER={jwtTokenIssuer}" "MOCK_UID={mockUid}"
 
 # Possibly move this to a separate init step
-yum install -y amazon-cloudwatch-agent
+#yum install -y amazon-cloudwatch-agent
 
 # Must match port used in express
 iptables -A INPUT -p tcp --dport 8080 -m state --state NEW -j ACCEPT
@@ -31,8 +31,7 @@ npm -v
 
 cd $HOME/source/packages/node-server
 
-whoami
 npm ci
 npm run build
-npm run start &
-echo npm running
+#npm run start &
+#echo npm running
