5501a066126e4ccad80cc027cd64cf559283ceab
noravux <tukiainen.jiri@gmail.com>
2023-08-07 11:40:33 +0300

    Only request page contents if not on search route

--- packages/frontend/src/hooks/query/GetCategoryPageContent.ts
+++ packages/frontend/src/hooks/query/GetCategoryPageContent.ts
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 import { QueryKeys } from '../../constants/QueryKeys';
 import { getRouterName } from '../../utils/helpers';
+import { Routes } from '../../constants/Routes';
 
 export const useGetCategoryPageContent = (categoryName: string) => {
   return useQuery({
-    enabled: Boolean(categoryName),
+    enabled: Boolean(categoryName) && categoryName !== Routes.SEARCH_RESULT.slice(1),
     queryKey: [QueryKeys.GET_PAGE_CONTENTS_QUERY_KEY, categoryName],
     queryFn: async () => {
       const response = await axios.get(`/api/database/page-contents/${getRouterName(categoryName)}`);
