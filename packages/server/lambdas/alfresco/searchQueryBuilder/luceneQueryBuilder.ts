10333ffc875900bb36b41901ccdc41c43ed7f948
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-08-30 15:03:55 +0300

    Fix/search query (#320)
    * fix search query

--- packages/server/lambdas/alfresco/searchQueryBuilder/luceneQueryBuilder.ts
+++ packages/server/lambdas/alfresco/searchQueryBuilder/luceneQueryBuilder.ts
@@ -79,16 +79,18 @@ export class LuceneQueryBuilder implements SearchQueryBuilder {
     return query;
   }
 
+  searchStringToArray(sentence: string) {
+    return sentence.split(' ');
+  }
+
   removeSpecialCharacters(sentence: string) {
     // Replace any special character (!?,._ etc.) with "?".
     // The ?-character is single character wildcad rather than * that could fill multiple characters.
-    return sentence.match(/[a-ö]|[0-9]|[-]|\s/gi)?.join(' ');
+    return sentence.match(/[a-ö]|[0-9]|[-]|\s/gi)?.join('');
   }
 
-  addWildcard(sentence: string) {
-    let parsedSentence = sentence;
-
-    parsedSentence = sentence.split(' ').join('*');
+  addWildcard(searchTerms: string[]) {
+    let parsedSentence = searchTerms.join('*');
     parsedSentence = `*${parsedSentence}*`;
 
     return parsedSentence;
@@ -98,17 +100,21 @@ export class LuceneQueryBuilder implements SearchQueryBuilder {
     const fileType = '+TYPE:"cm:content"';
     const defaultPathQuery = this.defaultPath ? `+PATH:\"${this.defaultPath}\"` : '';
     const searchTerm = this.removeSpecialCharacters(parameter.term);
-    const searchTermWildCard = this.addWildcard(searchTerm as string);
+    const searchTermWildCard = this.addWildcard(this.searchStringToArray(searchTerm as string));
+
+    const searchTermAmount = this.searchStringToArray(searchTerm as string).length;
+    // cannot use Lucene search proximity when querying only one word.
+    const searchProximity = searchTermAmount > 1 ? `~3` : `~`;
 
     // relevance level of matching documents based on the terms found
     // By default, the boost factor is 1. Although the boost factor must be positive, it can be less than 1
     // https://lucene.apache.org/core/2_9_4/queryparsersyntax.html#Boosting%20a%20Term
     const relevanceBoost = { text: 1, name: 4, title: 4, description: 2 };
 
-    const contentSearchQuery = `TEXT:\"${searchTerm}\"~6^${relevanceBoost.text}`;
-    const fileNameSearchQuery = `@cm\\:name:(${searchTermWildCard} OR ${searchTerm}~6)^${relevanceBoost.name}`;
-    const fileTitleSearchQuery = `@cm\\:title:(${searchTermWildCard} OR ${searchTerm}~6)^${relevanceBoost.title}`;
-    const descriptionSearchQuery = `@cm\\:description:(${searchTermWildCard} OR ${searchTerm}~6)^${relevanceBoost.description}`;
+    const contentSearchQuery = `TEXT:\"${searchTerm}\"${searchProximity}^${relevanceBoost.text}`;
+    const fileNameSearchQuery = `@cm\\:name:(\"${searchTermWildCard}\" OR \"${searchTerm}\"${searchProximity})^${relevanceBoost.name}`;
+    const fileTitleSearchQuery = `@cm\\:title:(\"${searchTermWildCard}\" OR \"${searchTerm}\"${searchProximity})^${relevanceBoost.title}`;
+    const descriptionSearchQuery = `@cm\\:description:(\"${searchTermWildCard}\" OR \"${searchTerm}\"${searchProximity})^${relevanceBoost.description}`;
 
     const extendedSearchQuery = `${fileNameSearchQuery} OR ${fileTitleSearchQuery} OR ${descriptionSearchQuery} OR ${contentSearchQuery}`;
 
