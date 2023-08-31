e53b3fa3d55ba09d12566322cddd3199855f3306
noravux <tukiainen.jiri@gmail.com>
2023-08-31 09:05:43 +0300

    Add new util to display readable name for areas

--- packages/frontend/src/pages/ProtectedPage/AreaPage/index.tsx
+++ packages/frontend/src/pages/ProtectedPage/AreaPage/index.tsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { PageTitleWrapper, ProtectedContainerWrapper } from '../../../styles/common';
+import { getReadableAreaTitle } from '../../../utils/mapUtil';
 
 export const AreaPage = () => {
   const { area } = useParams<{ area: string }>();
   return (
     <ProtectedContainerWrapper>
-      <PageTitleWrapper>{area}</PageTitleWrapper>
+      <PageTitleWrapper>{getReadableAreaTitle(area!)}</PageTitleWrapper>
     </ProtectedContainerWrapper>
   );
 };
