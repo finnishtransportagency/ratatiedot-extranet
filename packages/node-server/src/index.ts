3b24fde1ef7ee608448d45b002b3c6751c26de1f
noravux <tukiainen.jiri@gmail.com>
2023-09-04 14:35:13 +0300

    wip

--- packages/node-server/src/index.ts
+++ packages/node-server/src/index.ts
@@ -1,12 +1,29 @@
 import multer from 'multer';
 import express, { Request, Response } from 'express';
+import { log } from './utils/logger.js';
 import uploadFileHandler from './services/alfresco/upload-file.js';
+import { RataExtraEC2Error } from './utils/errors.js';
 
 const upload = multer().single('filedata');
 
 const uploadFile = async (req: Request, res: Response) => {
-  const result = await uploadFileHandler(req);
-  res.send(result);
+  try {
+    const result = await uploadFileHandler(req);
+    res.send(result);
+  } catch (err) {
+    log.error(err);
+    if (err.status) {
+      if (err.status === 409) {
+        const rataExtraEC2Error = new RataExtraEC2Error('File already exists', 409, 'fileAlreadyExists');
+        return res
+          .status(rataExtraEC2Error.statusCode)
+          .json({ errorTranslationKey: rataExtraEC2Error.errorTranslationKey });
+      }
+      res.sendStatus(err.status);
+    } else {
+      res.sendStatus(500);
+    }
+  }
 };
 
 const app = express();
