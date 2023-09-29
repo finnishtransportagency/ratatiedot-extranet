03ad530d3b727f718b94178ed1657f1d78e7131f
noravux <tukiainen.jiri@gmail.com>
2023-09-29 11:33:51 +0300

    wip

--- packages/server/lambdas/alfresco/list-activities.ts
+++ packages/server/lambdas/alfresco/list-activities.ts
@@ -8,6 +8,25 @@ import { getUser, validateReadUser } from '../../utils/userService';
 import { alfrescoApiVersion, alfrescoAxios } from '../../utils/axios';
 import { getNodes } from './list-nodes';
 
+export interface AlfrescoActivityResponse {
+  entry: {
+    postedAt: string;
+    feedPersonId: string;
+    postPersonId: string;
+    siteId: string;
+    activitySummary: {
+      firstName: string;
+      lastName: string;
+      parentObjectId: string;
+      title: string;
+      objectId: string;
+    };
+    id: string;
+    activityType: string;
+    parent: unknown;
+  };
+}
+
 export const getActivities = async (options: AxiosRequestConfig) => {
   try {
     const response = await alfrescoAxios.get(
@@ -19,6 +38,32 @@ export const getActivities = async (options: AxiosRequestConfig) => {
     throw error;
   }
 };
+
+async function combineChildWithParent(childData: AlfrescoActivityResponse[], options: AxiosRequestConfig) {
+  const combinedData: AlfrescoActivityResponse[] = [];
+  const parentPromises = [];
+
+  for (const child of childData) {
+    const parentId = child.entry.activitySummary.parentObjectId;
+
+    const parentPromise = getNodes(parentId, options).then((parent) => {
+      console.log('parent: ', parent);
+      if (parent) {
+        const combinedItem = {
+          ...child,
+          parent,
+        };
+        combinedData.push(combinedItem);
+      }
+    });
+
+    parentPromises.push(parentPromise);
+  }
+
+  await Promise.all(parentPromises);
+
+  return combinedData;
+}
 
 /**
  * Get the list of recent activities
@@ -36,34 +81,15 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
     const options = await getAlfrescoOptions(user.uid);
     const activityList = await getActivities(options);
 
-    const combinedData = [];
-    const parentNodePromises = [];
     const activityEntries = activityList.list.entries;
-    console.log('activityList: ', activityEntries);
 
-    for (const child of activityEntries) {
-      console.log('child: ', child);
-      const parentNodePromise = await getNodes(child.entry.activitySummary.parentObjectId, options).then((parent) => {
-        console.log('parent: ', parent);
-        if (parent) {
-          const combinedItem = {
-            ...child.entry,
-            parentNodeCategory: parent.name,
-          };
+    const combinedData = await combineChildWithParent(activityEntries, options);
 
-          console.log('combinedItem: ', combinedItem);
-          combinedData.push(combinedItem);
-        }
-      });
-      parentNodePromises.push(parentNodePromise);
-    }
-
-    const response = await Promise.all(parentNodePromises);
-
+    console.log('combinedData: ', combinedData);
     log.info(user, `response: ${JSON.stringify(activityList, null, 2)}`);
 
     const responseBody = {
-      data: response ?? {
+      data: combinedData ?? {
         list: {
           pagination: {
             count: 0,
