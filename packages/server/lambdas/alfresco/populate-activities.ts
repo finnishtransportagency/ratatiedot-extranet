f9543a3667c2dc0fc96da223b949a4e18e5a3afe
noravux <tukiainen.jiri@gmail.com>
2023-10-13 13:18:08 +0300

    wip

--- packages/server/lambdas/alfresco/populate-activities.ts
+++ packages/server/lambdas/alfresco/populate-activities.ts
@@ -1,20 +1,113 @@
+import { AxiosRequestConfig } from 'axios';
+import { alfrescoAxios, alfrescoApiVersion } from '../../utils/axios';
 import { getRataExtraLambdaError } from '../../utils/errors';
 import { log } from '../../utils/logger';
 import { getMockUser, validateReadUser } from '../../utils/userService';
-//import { DatabaseClient } from '../database/client';
+import { DatabaseClient } from '../database/client';
+import { AlfrescoActivityResponse, AlfrescoCombinedResponse } from '../database/get-activities';
+import { findEndpoint, getAlfrescoOptions } from '../../utils/alfresco';
+import { CategoryDataBase } from '@prisma/client';
 
-//const database = await DatabaseClient.build();
+const database = await DatabaseClient.build();
+let fileEndpointsCache: Array<CategoryDataBase> = [];
+
+const getActivities = async (options: AxiosRequestConfig, skipCount = 0, maxItems = 50) => {
+  try {
+    const response = await alfrescoAxios.get(
+      `${alfrescoApiVersion}/people/-me-/activities?skipCount=${skipCount}&maxItems=${maxItems}`,
+      options,
+    );
+
+    const activities = response.data.list.entries as AlfrescoActivityResponse[];
+    const nonDownloadActivities = activities.filter(
+      (child: { entry: { activityType: string } }) =>
+        child.entry.activityType !== 'org.alfresco.documentlibrary.file-downloaded' &&
+        child.entry.activityType !== 'org.alfresco.documentlibrary.folder-downloaded',
+    );
+    return nonDownloadActivities;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getNode = async (nodeId: string, options: AxiosRequestConfig, include: string[]) => {
+  try {
+    let queryParameter = '';
+    if (include.length) {
+      queryParameter = `?include=${include.join(',')}`;
+    }
+    const response = await alfrescoAxios.get(`${alfrescoApiVersion}/nodes/${nodeId}${queryParameter}`, options);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+async function combineData(childData: AlfrescoActivityResponse[], options: AxiosRequestConfig) {
+  const combinedData: AlfrescoCombinedResponse[] = [];
+  const nodePromises = [];
+
+  for (const child of childData) {
+    const nodeId = child.entry.activitySummary.objectId;
+
+    // get the contents of the node to determine its category
+    const nodePromise = getNode(nodeId, options, ['path']).then((node) => {
+      // eg. "/Company Home/Sites/site/root/category1"
+      // where category1 is the actual categoryName we want to know
+      const categoryname: string = node.entry.path.elements[4]?.name;
+      // If node has a category and category is not the root page
+      if (categoryname && categoryname !== 'documentLibrary') {
+        const combinedItem = {
+          activityEntry: child.entry,
+          nodeEntry: node.entry,
+          categoryName: categoryname,
+        };
+        combinedData.push(combinedItem);
+      }
+    });
+    nodePromises.push(nodePromise);
+  }
+
+  await Promise.all(nodePromises);
+  return combinedData;
+}
 
 /**
- * Get the list of recent activities
- * @returns {Promise<unknown>} List of activities
+ * Fetch activies from Alfresco, filter and save to db
+ * @returns {Promise<void>}
  */
 export async function handleRequest(): Promise<unknown> {
   try {
     const user = getMockUser();
     validateReadUser(user);
 
+    const options = await getAlfrescoOptions(user.uid);
+
     log.info(`Fetching alfresco activities and populating database..`);
+
+    if (!fileEndpointsCache.length) {
+      log.debug('Cache empty');
+      fileEndpointsCache = await database.categoryDataBase.findMany();
+    }
+
+    const activityList = await getActivities(options, 0, 5000);
+    const combinedData = await combineData(activityList, options);
+
+    for (const item of combinedData) {
+      const categoryData = findEndpoint(item.categoryName, fileEndpointsCache);
+      if (categoryData) {
+        await database.activity.create({
+          data: {
+            alfrescoId: item.nodeEntry.id,
+            action: item.activityEntry.activityType,
+            fileName: item.nodeEntry.name,
+            timestamp: item.activityEntry.postedAt,
+            mimeType: item.nodeEntry.content.mimeType,
+            categoryId: categoryData?.id,
+          },
+        });
+      }
+    }
 
     return {
       statusCode: 200,
--- packages/server/lambdas/database/get-activities.ts
+++ packages/server/lambdas/database/get-activities.ts
@@ -1,12 +1,12 @@
 import { ALBEvent, ALBResult } from 'aws-lambda';
-import { AxiosRequestConfig } from 'axios';
-import { getAlfrescoOptions } from '../../utils/alfresco';
 
 import { getRataExtraLambdaError } from '../../utils/errors';
 import { log } from '../../utils/logger';
 import { getUser, validateReadUser } from '../../utils/userService';
-import { alfrescoApiVersion, alfrescoAxios } from '../../utils/axios';
 import { AlfrescoEntry } from '../alfresco/fileRequestBuilder/types';
+import { DatabaseClient } from './client';
+
+const database = await DatabaseClient.build();
 
 export interface AlfrescoActivityResponse {
   entry: {
@@ -27,7 +27,7 @@ export interface AlfrescoActivityResponse {
   };
 }
 
-interface AlfrescoCombinedResponse {
+export interface AlfrescoCombinedResponse {
   activityEntry: {
     postedAt: string;
     feedPersonId: string;
@@ -42,88 +42,6 @@ interface AlfrescoCombinedResponse {
   categoryName: string;
 }
 
-const getActivities = async (options: AxiosRequestConfig, skipCount = 0) => {
-  try {
-    const response = await alfrescoAxios.get(
-      `${alfrescoApiVersion}/people/-me-/activities?skipCount=${skipCount}&maxItems=25`,
-      options,
-    );
-
-    const activities = response.data.list.entries as AlfrescoActivityResponse[];
-    const nonDownloadActivities = activities.filter(
-      (child: { entry: { activityType: string } }) =>
-        child.entry.activityType !== 'org.alfresco.documentlibrary.file-downloaded' &&
-        child.entry.activityType !== 'org.alfresco.documentlibrary.folder-downloaded',
-    );
-    return nonDownloadActivities;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getNode = async (nodeId: string, options: AxiosRequestConfig, include: string[]) => {
-  try {
-    let queryParameter = '';
-    if (include.length) {
-      queryParameter = `?include=${include.join(',')}`;
-    }
-    const response = await alfrescoAxios.get(`${alfrescoApiVersion}/nodes/${nodeId}${queryParameter}`, options);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const getNonDownloadActivities = async (options: AxiosRequestConfig) => {
-  const target = 5;
-  let skipCount = 0;
-  let results: AlfrescoActivityResponse[] = [];
-
-  while (results.length < target) {
-    const activities = await getActivities(options, skipCount);
-    results = results.concat(activities);
-
-    skipCount += 25;
-
-    if (results.length >= target) {
-      break;
-    }
-
-    // TODO: improve so that rare but possible endless loops are avoided
-  }
-  const nonDownloadActivities = results.slice(0, target);
-  return nonDownloadActivities;
-};
-
-async function combineData(childData: AlfrescoActivityResponse[], options: AxiosRequestConfig) {
-  const combinedData: AlfrescoCombinedResponse[] = [];
-  const nodePromises = [];
-
-  for (const child of childData) {
-    const nodeId = child.entry.activitySummary.objectId;
-
-    // get the contents of the node to determine its category
-    const nodePromise = getNode(nodeId, options, ['path']).then((node) => {
-      // eg. "/Company Home/Sites/site/root/category1"
-      // where category1 is the actual categoryName we want to know
-      const categoryname: string = node.entry.path.elements[4]?.name;
-      // If node has a category and category is not the root page
-      if (categoryname && categoryname !== 'documentLibrary') {
-        const combinedItem = {
-          activityEntry: child.entry,
-          nodeEntry: node.entry,
-          categoryName: categoryname,
-        };
-        combinedData.push(combinedItem);
-      }
-    });
-    nodePromises.push(nodePromise);
-  }
-
-  await Promise.all(nodePromises);
-  return combinedData;
-}
-
 /**
  * Get the list of recent activities
  * @param {ALBEvent} event
@@ -132,18 +50,14 @@ async function combineData(childData: AlfrescoActivityResponse[], options: Axios
 export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
   try {
     const user = await getUser(event);
+    validateReadUser(user);
 
     log.info(user, `Fetching recent Alfresco activity list`);
 
-    validateReadUser(user);
-
-    const options = await getAlfrescoOptions(user.uid);
-
-    const activityList = await getNonDownloadActivities(options);
-    const combinedData = await combineData(activityList, options);
+    const activities = await database.activity.findMany({ take: 5 });
 
     const responseBody = {
-      data: combinedData ?? {
+      data: activities ?? {
         list: {
           pagination: {
             count: 0,
