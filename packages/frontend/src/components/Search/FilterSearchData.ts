5990d38752e277a1f4dc6faf7a30f0d8a7337b1e
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2023-10-27 16:41:51 +0300

    remove unused constants (#348)

--- packages/frontend/src/components/Search/FilterSearchData.ts
+++ packages/frontend/src/components/Search/FilterSearchData.ts
@@ -1,8 +1,3 @@
-import { flatMapByKey, getTranslatedCategoryData } from '../../utils/helpers';
-import categoryData from '../../assets/data/FinnishCategories.json';
-import { FileFormats } from '../../constants/Data';
-import i18n from '../../i18n';
-
 export enum SearchParameterName {
   MIME = 'mime',
   CATEGORY = 'category',
@@ -20,22 +15,3 @@ export interface IItem {
   type: SearchParameterName;
   items: string[];
 }
-
-// Omit instruction pages from filter search
-const categoriesWithoutInstructions = categoryData.filter((entry) => {
-  const categoryKey = Object.keys(entry.category)[0];
-  return categoryKey !== 'INSTRUCTIONS';
-});
-
-export const FilterSearchData: IItem[] = [
-  {
-    name: i18n.t('search:format'),
-    type: SearchParameterName.MIME,
-    items: FileFormats,
-  },
-  {
-    name: i18n.t('search:category'),
-    type: SearchParameterName.CATEGORY,
-    items: flatMapByKey(getTranslatedCategoryData(categoriesWithoutInstructions), 'subCategories'),
-  },
-];
