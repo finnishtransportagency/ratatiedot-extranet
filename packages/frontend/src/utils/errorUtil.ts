c4041740dc3402e302084ea0ae5e198ce86f206a
noravux <tukiainen.jiri@gmail.com>
2023-11-01 14:17:10 +0200

    Don't provide default translation key for errors

--- packages/frontend/src/utils/errorUtil.ts
+++ packages/frontend/src/utils/errorUtil.ts
@@ -6,6 +6,10 @@ export const getErrorMessage = (error: any): string => {
   if (error instanceof AxiosError) {
     const errorTranslationKey = error.response?.data?.errorTranslationKey;
 
+    if (errorTranslationKey) {
+      return t(`apiErrors:${errorTranslationKey}` as any);
+    }
+
     switch (error.response?.status) {
       case 401:
         return t('common:error.401');
@@ -22,10 +26,6 @@ export const getErrorMessage = (error: any): string => {
         return t('common:error.500');
       case Errors.ERR_NETWORK:
         return t('common:error.networkError');
-    }
-
-    if (errorTranslationKey) {
-      return t(`apiErrors:${errorTranslationKey}` as any);
     }
   }
 
