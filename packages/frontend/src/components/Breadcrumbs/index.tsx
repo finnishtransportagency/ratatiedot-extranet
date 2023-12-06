ce680e59473f029bf56c75e2cd4c0d65399deb3c
noravux <tukiainen.jiri@gmail.com>
2023-12-06 15:51:24 +0200

    Don't show notice ID in breadcrumbs

--- packages/frontend/src/components/Breadcrumbs/index.tsx
+++ packages/frontend/src/components/Breadcrumbs/index.tsx
@@ -1,6 +1,6 @@
 import { Breadcrumbs, Link, Stack, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useMatch, useParams } from 'react-router-dom';
 import { Colors } from '../../constants/Colors';
 import { parseRouterName } from '../../utils/helpers';
 import { getReadableAreaTitle } from '../../utils/mapUtil';
@@ -10,7 +10,7 @@ export const CustomBreadcrumbs = () => {
   const { pathname } = useLocation();
   const routerNames = pathname.split('/').slice(1);
   const { area } = useParams<{ area: string }>();
-
+  const matchNoticeRoute = useMatch('/ajankohtaista/:id/:date');
   const BreadcrumbText = ({ routerName }: { routerName: string }) => {
     return (
       <Typography sx={{ color: Colors.extrablack }}>
@@ -23,9 +23,9 @@ export const CustomBreadcrumbs = () => {
     breadcrumbPath += '/' + routerName;
     return index === 0 ? (
       <BreadcrumbText key={index} routerName={routerName} />
-    ) : (
+    ) : index === 1 && matchNoticeRoute ? null : (
       <Link underline="hover" key={index} href={breadcrumbPath}>
-        <BreadcrumbText routerName={routerName} />
+        <Typography sx={{ color: Colors.extrablack }}>{routerName}</Typography>
       </Link>
     );
   });
