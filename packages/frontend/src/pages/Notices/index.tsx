c895f6512079e6b3cfdbf42521199b04faf584c6
noravux <tukiainen.jiri@gmail.com>
2023-12-07 11:46:59 +0200

    Add notice deletion functionality

--- packages/frontend/src/pages/Notices/index.tsx
+++ packages/frontend/src/pages/Notices/index.tsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react';
 import { getNotices } from '../../services/NoticeListService';
-import { ProtectedContainerWrapper } from '../../styles/common';
-import { Box, Button, List, ListItemButton, ListItemText, Pagination, Typography } from '@mui/material';
+import { ButtonWrapper, ProtectedContainerWrapper } from '../../styles/common';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Pagination,
+  Typography,
+} from '@mui/material';
 import { format } from 'date-fns';
 import { Colors } from '../../constants/Colors';
 import { DateFormat, URIFriendlyDateFormat } from '../../constants/Formats';
@@ -11,15 +22,22 @@ import { ErrorMessage } from '../../components/Notification/ErrorMessage';
 import { Routes } from '../../constants/Routes';
 import { t } from 'i18next';
 import { checkAdminRights } from '../../services/AdminRightService';
-import { theme } from '../../styles/createTheme';
+import { DeleteOutline } from '@mui/icons-material';
+import { Modal } from '../../components/Modal/Modal';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { deleteNotice } from '../../services/NoticeDeleteService';
 
 export const Notices = () => {
   const [notices, setNotices] = useState<Notice[]>([]);
-  const [error, setError] = useState();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+  const [success, setSuccess] = useState(false);
   const [page, setPage] = useState(1);
   const navigate = useNavigate();
   const [totalPages, setTotalPages] = useState(0);
   const [isUserAdmin, setIsUserAdmin] = useState(false);
+  const [open, setIsOpen] = useState(false);
+  const [selectedNotice, setSelectedNotice] = useState<Notice>();
 
   useEffect(() => {
     const fetchNotices = async () => {
@@ -29,7 +47,8 @@ export const Notices = () => {
         setNotices(notices);
         setTotalPages(Math.ceil(totalItems / 10));
       } catch (error: any) {
-        setError(error);
+        setError(true);
+        setErrorMessage(error);
       }
     };
 
@@ -46,45 +65,119 @@ export const Notices = () => {
     setPage(value);
   };
 
-  if (error) return <ErrorMessage error={error} />;
+  const handleDeleteNotice = async () => {
+    await deleteNotice(selectedNotice?.id!)
+      .then((result) => {
+        setError(false);
+        setSuccess(true);
+        console.log('res from delete: ', result);
+        setIsOpen(false);
+
+        setNotices((prevNotices) => {
+          return prevNotices.filter((notice) => notice.id !== selectedNotice?.id);
+        });
+      })
+      .catch((error) => {
+        setSuccess(false);
+        setError(true);
+      });
+  };
+
+  if (error) return <ErrorMessage error={errorMessage} />;
 
   return (
-    <ProtectedContainerWrapper>
-      {isUserAdmin && (
-        <Button variant="outlined" sx={{ mb: '16px' }} onClick={() => navigate(Routes.NEW_NOTICE)}>
-          {t('common:noticeList.createNewNotice')}
-        </Button>
-      )}
-      <List>
-        {notices.map((node) => {
-          return (
-            <ListItemButton
-              sx={{
-                whiteSpace: 'normal',
-                flexGrow: 'unset',
-                '&.MuiListItemButton-root:hover': {
-                  backgroundColor: theme.palette.action.hover,
-                },
-              }}
-              key={node.id}
-              href={`${Routes.NOTICES}/${node.id}/${format(new Date(node.createdTime), URIFriendlyDateFormat)}`}
-            >
-              <ListItemText
-                secondary={format(new Date(node.createdTime), DateFormat)}
-                primary={node.content[0].children[0].text}
-              />
-            </ListItemButton>
-          );
-        })}
-      </List>
-      <Pagination
-        count={totalPages}
-        page={page}
-        onChange={(_, value) => handlePageChange(value)}
-        sx={{ justifyContent: 'center', display: 'flex' }}
-        showFirstButton
-        showLastButton
-      />
-    </ProtectedContainerWrapper>
+    <>
+      <ProtectedContainerWrapper>
+        {isUserAdmin && (
+          <Button variant="outlined" sx={{ mb: '16px' }} onClick={() => navigate(Routes.NEW_NOTICE)}>
+            {t('common:noticeList.createNewNotice')}
+          </Button>
+        )}
+        <List>
+          {notices.map((node) => {
+            return (
+              <ListItem
+                key={node.id}
+                secondaryAction={
+                  isUserAdmin && (
+                    <IconButton
+                      edge="end"
+                      onClick={() => {
+                        setIsOpen(true);
+                        setSelectedNotice(node);
+                      }}
+                    >
+                      <DeleteOutline />
+                    </IconButton>
+                  )
+                }
+                disablePadding
+              >
+                <ListItemButton
+                  key={node.id}
+                  href={`${Routes.NOTICES}/${node.id}/${format(new Date(node.createdTime), URIFriendlyDateFormat)}`}
+                >
+                  <ListItemText
+                    secondary={format(new Date(node.createdTime), DateFormat)}
+                    primary={node.content[0].children[0].text}
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+        <Pagination
+          count={totalPages}
+          page={page}
+          onChange={(_, value) => handlePageChange(value)}
+          sx={{ justifyContent: 'center', display: 'flex' }}
+          showFirstButton
+          showLastButton
+        />
+      </ProtectedContainerWrapper>
+      <Modal
+        open={open}
+        handleClose={() => setIsOpen(false)}
+        onSnackbarClose={() => {
+          setError(false);
+          setSuccess(false);
+        }}
+        title={t('common:noticeList.deleteNotice')}
+        error={error}
+        success={success}
+        errorMessage={t('common:noticeList.deleteNoticeFailed')}
+        successMessage={t('common:noticeList.deleteNoticeSuccess')}
+        children={
+          <Box>
+            <Typography>{`${t('common:noticeList.confirmDeleteNotice')}`}</Typography>
+            <Box sx={{ display: 'flex' }}>
+              <ButtonWrapper
+                sx={{ marginLeft: 'auto' }}
+                color="primary"
+                variant="text"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('common:action.cancel')}
+              </ButtonWrapper>
+              <ButtonWrapper
+                color="error"
+                variant="contained"
+                disabled={false}
+                onClick={() => handleDeleteNotice()}
+                startIcon={
+                  false ? (
+                    <CircularProgress sx={{ color: Colors.darkgrey }} size="16px"></CircularProgress>
+                  ) : (
+                    <DeleteOutlineOutlinedIcon></DeleteOutlineOutlinedIcon>
+                  )
+                }
+              >
+                {t('common:action.delete')}
+              </ButtonWrapper>
+            </Box>
+          </Box>
+        }
+      ></Modal>
+    </>
   );
 };
