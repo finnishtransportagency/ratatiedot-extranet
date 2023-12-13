75480810762f429b6da1e9d66ec67b6f287eef93
noravux <tukiainen.jiri@gmail.com>
2023-12-13 13:04:53 +0200

    wip

--- packages/server/lambdas/s3/upload-image.ts
+++ packages/server/lambdas/s3/upload-image.ts
@@ -1,22 +1,48 @@
 import AWS from 'aws-sdk';
+import { getUser, validateAdminUser } from '../../utils/userService';
+import { ALBEvent, ALBResult } from 'aws-lambda';
+import { getRataExtraLambdaError } from '../../utils/errors';
+import { log, auditLog } from '../../utils/logger';
 
 const s3 = new AWS.S3();
 const RATAEXTRA_STACK_IDENTIFIER = process.env.RATAEXTRA_STACK_IDENTIFIER;
 
-export const uploadImage = async (file: Buffer, fileName: string): Promise<string> => {
-  const params = {
-    Bucket: `s3-${RATAEXTRA_STACK_IDENTIFIER}-images`,
-    Key: fileName,
-    Body: file,
-    ACL: 'private',
-  };
-
+/**
+ * Upload image to S3 Example request: /api/images
+ * @param {ALBEvent} event
+ * @param {{string}} event.path Path should end with the page to upload the file to
+ * @param {{string}} event.body File contents and metadata to upload
+ * @returns  {Promise<any>} JSON stringified URL of the image
+ */
+export async function handleRequest(event: ALBEvent): Promise<ALBResult | undefined> {
   try {
+    //const paths = event.path.split('/');
+
+    const user = await getUser(event);
+    log.info(user, `Uploading image`);
+
+    console.log('EVENT AT LAMBDA: ', event);
+
+    validateAdminUser(user);
+
+    const params = {
+      Bucket: `s3-${RATAEXTRA_STACK_IDENTIFIER}-images`,
+      Key: 'fileName',
+      Body: 'file',
+      ACL: 'private',
+    };
+
     await s3.upload(params).promise();
-    const url = `https://${RATAEXTRA_STACK_IDENTIFIER}-images.s3.eu-west-1.amazonaws.com/${fileName}`;
-    return url;
-  } catch (error) {
-    console.error('Error uploading file to S3:', error);
-    throw error;
+    const imageUrl = `https://${RATAEXTRA_STACK_IDENTIFIER}-images.s3.eu-west-1.amazonaws.com/${'fileName'}`;
+
+    auditLog.info(user, `Uploaded image`);
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type:': 'application/json' },
+      body: JSON.stringify(imageUrl),
+    };
+  } catch (err) {
+    log.error(err);
+    return getRataExtraLambdaError(err);
   }
-};
+}
