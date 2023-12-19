303daaaf5db04b980baae359bb9f55249864ec29
noravux <tukiainen.jiri@gmail.com>
2023-12-19 14:34:44 +0200

    Generate signed URL for image and return to client

--- packages/server/lambdas/database/get-notice.ts
+++ packages/server/lambdas/database/get-notice.ts
@@ -1,3 +1,4 @@
+const AWS = require('aws-sdk'); //eslint-disable-line @typescript-eslint/no-var-requires
 import { ALBEvent, ALBResult } from 'aws-lambda';
 
 import { RataExtraLambdaError, getRataExtraLambdaError } from '../../utils/errors';
@@ -6,6 +7,9 @@ import { getUser, validateAdminUser, validateReadUser } from '../../utils/userSe
 import { DatabaseClient } from './client';
 
 const database = await DatabaseClient.build();
+const keyPairId = process.env.CLOUDFRONT_SIGNER_PUBLIC_KEY_ID || '';
+const privateKey = process.env.SSM_CLOUDFRONT_SIGNER_PRIVATE_KEY || '';
+const cloudfront = new AWS.CloudFront.Signer(keyPairId, privateKey);
 
 /**
  * Get single notice by id. Example request: /api/notice/:id
@@ -26,6 +30,14 @@ export async function handleRequest(event: ALBEvent): Promise<ALBResult> {
     validateReadUser(user);
 
     const notice = await database.notice.findUnique({ where: { id: noticeId } });
+    const imageElement = notice.content.find((element) => element.type === 'image');
+    if (imageElement) {
+      const signedUrl = await cloudfront.getSignedUrl({
+        url: `https://dawlcrdphn1az.cloudfront.net/images/${imageElement.url}`,
+        expires: Math.floor(Date.now() / 1000 + 60),
+      });
+      imageElement.url = signedUrl;
+    }
 
     if ((notice?.publishTimeStart as Date) > new Date()) {
       validateAdminUser(user);
