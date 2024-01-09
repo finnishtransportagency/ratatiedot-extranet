78e5fb79c85683cfb514cc87ef7056e801f62176
noravux <tukiainen.jiri@gmail.com>
2024-01-09 10:47:15 +0200

    Add dialog for editing nodes

---
+++ packages/frontend/src/components/Files/FileEditDialogButton.tsx
@@ -0,0 +1,55 @@
+import { Button, ButtonProps } from '@mui/material';
+import { AxiosResponse } from 'axios';
+import { FunctionComponent, useState } from 'react';
+
+import { FileEditDialog } from './FileEditDialog';
+import { TNode } from '../../types/types';
+import { EditOutlined } from '@mui/icons-material';
+import { useTranslation } from 'react-i18next';
+
+interface DialogButtonProps {
+  categoryName: string;
+  node: TNode | null;
+  buttonProps?: ButtonProps;
+  onUpload?: (response: AxiosResponse) => any;
+  disabled?: boolean;
+}
+
+export const FileEditDialogButton: FunctionComponent<DialogButtonProps> = ({
+  categoryName,
+  node,
+  onUpload,
+  disabled,
+}) => {
+  const [openFileEditDialog, setOpenFileEditDialog] = useState(false);
+  const { t } = useTranslation(['common']);
+  const handleOpenFileEditDialog = () => {
+    setOpenFileEditDialog(true);
+  };
+  const handleCloseFileEditDialog = () => {
+    setOpenFileEditDialog(false);
+  };
+
+  const handleUpload = (result: AxiosResponse) => {
+    if (onUpload) onUpload(result);
+  };
+
+  return (
+    <>
+      <Button variant="outlined" disabled={disabled} startIcon={<EditOutlined />} onClick={handleOpenFileEditDialog}>
+        {t('edit.edit')}
+      </Button>
+      {node?.entry ? (
+        <FileEditDialog
+          node={node}
+          onClose={handleCloseFileEditDialog}
+          onUpload={handleUpload}
+          open={openFileEditDialog}
+          categoryName={categoryName}
+        ></FileEditDialog>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
