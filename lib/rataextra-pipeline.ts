17a3e92c8aa5f632a560d8e19eba60db0eea9534
noravux <tukiainen.jiri@gmail.com>
2024-02-14 14:15:02 +0200

    Update sonarscanner version

--- lib/rataextra-pipeline.ts
+++ lib/rataextra-pipeline.ts
@@ -129,7 +129,7 @@ export class RataExtraPipelineStack extends Stack {
           },
         },
         installCommands: [
-          'export SONAR_SCANNER_VERSION=4.7.0.2747',
+          'export SONAR_SCANNER_VERSION=5.0.1.3006',
           'export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux',
           'curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip',
           'unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/',
