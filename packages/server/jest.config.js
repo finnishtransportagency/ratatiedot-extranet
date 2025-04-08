159d4fce1cd246fbfaffc210006e32a6f528b897
Santeri Pigg <34033265+saneDG@users.noreply.github.com>
2025-04-08 18:47:08 +0300

    fix test

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('jest').Config} */
+// import '@testing-library/jest-dom/extend-expect';
+
 const config = {
   verbose: true,
-  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
-  testPathIgnorePatterns: ['/node_modules/', '__mocks__'],
 };
 
 export default config;
