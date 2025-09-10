7647c9a430f2afa057e0edd444913900a3f98cf2
Yessergire Mohamed <yessergire.mohamed@nordcloud.com>
2025-09-10 08:43:28 +0300

    feat: Reduce Sentry trace sampling for production

--- packages/server/lambdas/handler-wrapper.ts
+++ packages/server/lambdas/handler-wrapper.ts
@@ -1,16 +1,17 @@
 import * as Sentry from '@sentry/aws-serverless';
 import { Handler } from 'aws-lambda';
+import { ENVIRONMENTS } from '../../../lib/config';
 
-if (process.env.ENVIRONMENT !== 'local') {
+if (process.env.ENVIRONMENT !== ENVIRONMENTS.local) {
   Sentry.init({
     dsn: 'https://992023a881caa64f7a5a41026b4ddf7b@o1193385.ingest.us.sentry.io/4509672722006016',
     environment: process.env.ENVIRONMENT || 'dev',
-    tracesSampleRate: 1.0,
+    tracesSampleRate: process.env.ENVIRONMENT == ENVIRONMENTS.prod ? 0.1 : 1.0,
   });
 }
 
 export const handlerWrapper = <T extends Handler>(handler: T): T => {
-  if (process.env.ENVIRONMENT === 'local') {
+  if (process.env.ENVIRONMENT === ENVIRONMENTS.local) {
     return handler;
   }
   return Sentry.wrapHandler(handler) as T;
