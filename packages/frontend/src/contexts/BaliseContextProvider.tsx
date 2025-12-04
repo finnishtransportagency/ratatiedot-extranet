a1e154b5d0b2ea5ef78a1ba47903838b467760e1
Andrea Peltokorpi <andrea.peltokorpi@nordcloud.com>
2025-12-04 14:51:55 +0200

    feat: implement BaliseContextProvider and update routes to use it

---
+++ packages/frontend/src/contexts/BaliseContextProvider.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { AppBarContextProvider } from './AppBarContext';
+import { MenuContextProvider } from './MenuContext';
+import { CategoryDataContextProvider } from './CategoryDataContext';
+import { ErrorContextProvider } from './ErrorContext';
+
+const providers = [AppBarContextProvider, MenuContextProvider, CategoryDataContextProvider, ErrorContextProvider];
+
+const combineComponents = (...components: FC[]) => {
+  return components.reduce(
+    (AccumulatedComponents: any, CurrentComponent: any) => {
+      return ({ children }: any): JSX.Element => {
+        return (
+          <AccumulatedComponents>
+            <CurrentComponent>{children}</CurrentComponent>
+          </AccumulatedComponents>
+        );
+      };
+    },
+    ({ children }: any) => <>{children}</>,
+  );
+};
+
+export const BaliseContextProvider = combineComponents(...providers);
