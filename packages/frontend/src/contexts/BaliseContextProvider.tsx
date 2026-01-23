9b2e89905f304eda70b3858637d090479d9587f8
Andrea Peltokorpi <andrea.peltokorpi@nordcloud.com>
2026-01-23 14:24:28 +0200

    feat: implement balise permissions check, service and context provider

--- packages/frontend/src/contexts/BaliseContextProvider.tsx
+++ packages/frontend/src/contexts/BaliseContextProvider.tsx
@@ -3,8 +3,15 @@ import { AppBarContextProvider } from './AppBarContext';
 import { MenuContextProvider } from './MenuContext';
 import { CategoryDataContextProvider } from './CategoryDataContext';
 import { ErrorContextProvider } from './ErrorContext';
+import { BalisePermissionsProvider } from './BalisePermissionsContext';
 
-const providers = [AppBarContextProvider, MenuContextProvider, CategoryDataContextProvider, ErrorContextProvider];
+const providers = [
+  AppBarContextProvider,
+  MenuContextProvider,
+  CategoryDataContextProvider,
+  ErrorContextProvider,
+  BalisePermissionsProvider,
+];
 
 const combineComponents = (...components: FC[]) => {
   return components.reduce(
