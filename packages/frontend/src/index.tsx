4a1a013036b37369cbd7fd885a176ecf1c860054
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-01-27 09:41:18 +0200

    chore: remove webvitals left from CRA

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from '@mui/material';
 
 import './index.css';
 import 'leaflet/dist/leaflet.css';
-import reportWebVitals from './reportWebVitals';
 import { router } from './routes';
 import { theme } from './styles/createTheme';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -25,8 +24,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
