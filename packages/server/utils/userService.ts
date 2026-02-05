5fe4a687ed494468eb151a063277866f76d8714a
Andrea Peltokorpi <andrea.peltokorpi@nordcloud.com>
2026-02-05 13:53:21 +0200

    test: remove write role from getMockUser function

--- packages/server/utils/userService.ts
+++ packages/server/utils/userService.ts
@@ -46,7 +46,7 @@ export function parseRoles(roles: string): string[] | undefined {
 
 export const getMockUser = (): RataExtraUser => ({
   uid: MOCK_UID,
-  roles: [STATIC_ROLES.read, STATIC_ROLES.admin, BALISE_ROLES.read, BALISE_ROLES.write],
+  roles: [STATIC_ROLES.read, STATIC_ROLES.admin, BALISE_ROLES.read],
   isMockUser: true,
 });
 
