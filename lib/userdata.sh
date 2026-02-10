5e5d386b44989bf970246c9bb2505b4e717f9d19
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-02-10 14:10:48 +0200

    chore: update node-server node version

--- lib/userdata.sh
+++ lib/userdata.sh
@@ -23,8 +23,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install v22.16.0
-nvm use v22.16.0
+nvm install v22.22.0
+nvm use v22.22.0
 nvm -v
 node -v
 npm -v
