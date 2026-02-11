37015172a70593cc90d038ac5ca81dda2d4c691d
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-02-11 09:24:34 +0200

    fix: convert prisma imports to .js

--- packages/node-server/src/services/database/client/index.ts
+++ packages/node-server/src/services/database/client/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../../../generated/prisma/client';
+import { PrismaClient } from '../../../../generated/prisma/client.js';
 import { ENVIRONMENTS } from './../../../utils/lib.js';
 import { getParameter, getSecuredStringParameter } from '../../../utils/parameterStore.js';
 
